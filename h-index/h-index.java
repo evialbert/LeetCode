5ee754719f656b19e8b639fc9582d2ef454b408a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-18 21:19:33 +0200

    Time: 0 ms (100.00%), Space: 37 MB (50.93%) - LeetHub

---
+++ h-index/h-index.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // counting papers for each citation number
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // finding the h-index
+        int k = n;
+        for (int s = papers[n]; k > s; s += papers[k])
+            k--;
+        return k;
+    }
+}
