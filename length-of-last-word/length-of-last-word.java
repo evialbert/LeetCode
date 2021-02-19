13b22dfb489179aa356972c7214d237bad9f16fa
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-19 18:20:28 +0200

    Time: 0 ms (100.00%), Space: 37.4 MB (62.85%) - LeetHub

---
+++ length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      int len = 0;
+        s = s.trim();
+        for (int i = s.length() - 1; i >=0; i --){
+            if (s.charAt(i) == ' '){
+                break;
+            }
+            else{
+                len ++;
+            }
+        }
+        return len;
+      
+    }
+}
