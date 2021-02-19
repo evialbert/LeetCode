c6fa6092db9af23481f89ac5f4aba69d3ee35805
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-19 22:40:40 +0200

    Time: 0 ms (100.00%), Space: 39.2 MB (59.63%) - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index=0;
+        for(int i=m;i<m+n;i++) {
+        		nums1[i]=nums2[index++];
+        }
+        Arrays.sort(nums1);
+    }
+}
