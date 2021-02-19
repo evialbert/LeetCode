8cf0aa573d652c7e80f6479b2b1438ff63b0f857
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-19 17:20:31 +0200

    Time: 0 ms (100.00%), Space: 37.6 MB (72.88%) - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        }else{
+            return -1;
+        }
+    }
+}
