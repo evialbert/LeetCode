2c338354cbb3d41765e27f294b8286e7531ec09a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-19 23:25:26 +0200

    Time: 48 ms (44.93%), Space: 14.3 MB (67.91%) - LeetHub

---
+++ triangle/triangle.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        n = len(triangle)
+        dp = triangle[-1]
+        for layer in range(n - 2, -1, -1):
+            for i in range(layer + 1):
+                dp[i] = min(dp[i], dp[i + 1]) + triangle[layer][i]
+        return dp[0]
