15fba28ea31b68b68f9dcec732ac10c52b07751a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-19 17:23:07 +0200

    Time: 28 ms (95.82%), Space: 14 MB (66.05%) - LeetHub

---
+++ longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s_len, stack = len(s), list()
+        for i, ch in enumerate(s):
+            if ch == '(':
+                stack.append(i)
+            elif stack and s[stack[-1]] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+
+        if not stack:
+            return s_len
+
+        stack = [-1] + stack + [s_len]
+        for i,val in enumerate(stack[1:]):
+            stack[i] = val - stack[i] - 1
+
+        return max(stack[:-1])
