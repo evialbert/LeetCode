cb615125c13bafad9e98003777a86547920252bd
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-20 16:03:34 +0200

    Time: 28 ms (96.38%), Space: 14.4 MB (71.78%) - LeetHub

---
+++ word-search-ii/word-search-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        def dfs(x, y, root):
+            letter = board[x][y]
+            cur = root[letter]
+            word = cur.pop('#', False)
+            if word:
+                res.append(word)
+            board[x][y] = '*'
+            for dirx, diry in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                curx, cury = x + dirx, y + diry
+                if 0 <= curx < m and 0 <= cury < n and board[curx][cury] in cur:
+                    dfs(curx, cury, cur)
+            board[x][y] = letter
+            if not cur:
+                root.pop(letter)
+                
+        trie = {}
+        for word in words:
+            cur = trie
+            for letter in word:
+                cur = cur.setdefault(letter, {})
+            cur['#'] = word
+        m, n = len(board), len(board[0])
+        res = []
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in trie:
+                    dfs(i, j, trie)
+        return res
