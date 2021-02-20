c6c063859641f58476c72573ccd58465d0c9f437
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-20 15:52:42 +0200

    Time: 264 ms (15.93%), Space: 14.8 MB (86.50%) - LeetHub

---
+++ course-schedule/course-schedule.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def canFinish(self, N, prerequisites):
+        """
+        :type N,: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = collections.defaultdict(list)
+        indegrees = collections.defaultdict(int)
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegrees[u] += 1
+        for i in range(N):
+            zeroDegree = False
+            for j in range(N):
+                if indegrees[j] == 0:
+                    zeroDegree = True
+                    break
+            if not zeroDegree: return False
+            indegrees[j] = -1
+            for node in graph[j]:
+                indegrees[node] -= 1
+        return True             
