674625408c6c7ef92aea7e8f5e4d0cbb26ffaf48
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-20 13:37:17 +0200

    Time: 88 ms (85.33%), Space: 31.2 MB (82.00%) - LeetHub

---
+++ reorder-list/reorder-list.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
+        """
+        if head and head.next and head.next.next:
+            #find mid
+            fast, slow = head, head
+            while fast.next and fast.next.next:
+                fast = fast.next.next
+                slow = slow.next
+            head1 = head
+            head2 = slow.next
+            slow.next = None
+
+            # reverse linked list head2     
+            dummy = ListNode(0)
+            dummy.next = head2
+            p = head2.next
+            head2.next = None
+            while p:
+                temp = p
+                p = p.next
+                temp.next = dummy.next
+                dummy.next = temp
+            head2 = dummy.next
+
+            # merge two linked list head1 and head2
+            p1 = head1
+            p2 = head2
+            while p2:
+                temp1 = p1.next
+                temp2 = p2.next
+                p1.next = p2
+                p2.next = temp1
+                p1 = temp1
+                p2 = temp2
