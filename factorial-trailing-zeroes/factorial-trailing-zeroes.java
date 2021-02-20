8285d7359493fe70d2d9c796d64a70b340c63d1f
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-20 14:38:38 +0200

    Time: 0 ms (100.00%), Space: 35.6 MB (88.98%) - LeetHub

---
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+   public int trailingZeroes(int n) {
+        
+        if (n == 0) {
+            return 0;
+        }
+        
+        return n / 5 + trailingZeroes(n / 5);
+        
+    }
+}
