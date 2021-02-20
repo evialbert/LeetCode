2b0cbf5cf2ab0f65e8b175140139f64baa81faa7
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-20 13:01:58 +0200

    Time: 1 ms (96.29%), Space: 39.4 MB (46.08%) - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,23 @@
+class Solution {
+   public int singleNumber(int[] nums) {
+
+    
+
+        int num = 0;
+
+    
+
+        for(int i=0; i<nums.length; i++){
+
+            num ^= nums[i]; 
+
+        }
+
+        
+
+        return num;
+
+   
+
+    }
+}
