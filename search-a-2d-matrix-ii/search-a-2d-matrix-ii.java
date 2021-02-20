bd988cb31e60f796eee0abef28b639c9860d91c2
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-20 18:02:12 +0200

    Time: 10 ms (19.98%), Space: 51.2 MB (15.35%) - LeetHub

---
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix==null || matrix.length<1 || matrix[0].length<1){
+            return false;
+        }
+        
+        int col = matrix[0].length-1;
+        int row = 0;
+        
+        while(col>=0 && row<matrix.length){
+            if(matrix[row][col]==target){
+                return true;
+            } else if(target<matrix[row][col]){
+                col--;
+            } else {
+                row++;
+            }
+        }
+        
+        return false;
+    }
+}
