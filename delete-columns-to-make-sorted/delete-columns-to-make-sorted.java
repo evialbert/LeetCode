160f89b0bd658f6e219b9ba97da835d237750375
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-21 16:41:36 +0200

    Time: 6 ms (89.37%), Space: 39.4 MB (39.46%) - LeetHub

---
+++ delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minDeletionSize(String[] A) {
+        int ans = 0;
+        for (int c = 0; c < A[0].length(); ++c)
+            for (int r = 0; r < A.length - 1; ++r)
+                if (A[r].charAt(c) > A[r+1].charAt(c)) {
+                    ans++;
+                    break;
+                }
+
+        return ans;
+    }
+}
