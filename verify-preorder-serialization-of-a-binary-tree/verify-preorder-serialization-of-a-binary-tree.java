9de5d5e21242358e5329163f0cc0012f834e9c66
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-02-28 17:00:59 +0200

    Time: 0 ms (100.00%), Space: 37.4 MB (98.02%) - LeetHub

---
+++ verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        int result = validatePreorderSerialization(preorder, 0);
+		//--if the return value is -1 or
+        //if the return number is less than length of the input means, the entire array is not iterated hence result should be false
+    	if(result == -1 || preorder.length()-1 != result){
+            return false;
+        }
+        else{
+            return true;
+        }
+	}
+
+	private static int validatePreorderSerialization(String preorder, int i) {
+		if(preorder.charAt(i) == '#') {
+			return i;
+		}
+		int in = pushuntilnextComma(preorder, i);
+		
+		if(in == -1 || i > preorder.length()-1) {
+			return -1;
+		}
+		//in the preorder, the node next to root is left subtree root, 
+		//hence, we will verify if the left subtree preorder serialized or not by passing next immediate(after comma) element
+		int left = validatePreorderSerialization(preorder, in);
+		if(left == -1) {
+			return -1;
+		}
+		left = pushuntilnextComma(preorder, left);
+		if(left == -1 || left > preorder.length()-1) {
+			return -1;
+		}
+		//the left check will return when the after the complete subtree is over and the returned index will be root(starting point) of the right subtree 
+		/* this is to verify if right sub tree is preorder serialized */
+		int right = validatePreorderSerialization(preorder, left);
+		return right;
+	}
+	
+	/* Incase of there are double digit numbers then push them until we reach next comma */
+	private static int pushuntilnextComma(String preorder, int i) {
+		while(preorder.charAt(i) != ',') {
+			++i;
+			if(i > preorder.length()-1) return -1;
+		}
+		return i+1;
+	}
+}
