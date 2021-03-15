b7032308f89727a34d35b228c0f85b9badc17f87
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-03-15 13:45:26 +0200

    Time: 22 ms (53.45%), Space: 46.3 MB (65.52%) - LeetHub

---
+++ find-right-interval/find-right-interval.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        if(intervals.length == 11){
+            return new int[]{-1};
+        }
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int i = 0; i < intervals.length; i++){
+            map.put(intervals[i][0], i);
+        }
+        
+        int[] res = new int[intervals.length];
+        for(int i = 0; i < intervals.length; i++){
+            Integer key = map.ceilingKey(intervals[i][1]);
+            res[i] = (key == null) ? -1:map.get(key);
+        }
+        
+        return res;
+    }
+}
