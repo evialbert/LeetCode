b5d3bd12d35f4c79c972256f92886894378fd8df
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-03-28 20:22:18 +0300

    Time: 0 ms (100.00%), Space: 36.6 MB (25.19%) - LeetHub

---
+++ predict-the-winner/predict-the-winner.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (i == j) {
+                    dp[j] = nums[i];
+                } else {
+                    dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+                }
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+}
