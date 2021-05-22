9c9dc4b8b971930ab8d105720eeb3b4e0ee87172
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-05-22 19:08:59 +0300

    Time: 0 ms (100.00%), Space: 35.4 MB (96.00%) - LeetHub

---
+++ bulb-switcher-ii/bulb-switcher-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int flipLights(int n, int m) {
+        if (n == 0) return 0;
+        if (m == 0) return 1;
+        if (n == 1) return 2;
+        if (n == 2 && m == 1) return 3;
+        if (n == 2) return 4;
+        if (m == 1) return 4;
+        if (m == 2) return 7;
+       return 8;
+    }
+}
