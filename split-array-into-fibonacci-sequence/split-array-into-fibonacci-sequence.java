d8e13a998c06250d91ccb060ae5f955b9d2c5f7a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2021-07-17 11:53:09 +0300

    Time: 1 ms (97.84%), Space: 37.2 MB (89.49%) - LeetHub

---
+++ split-array-into-fibonacci-sequence/split-array-into-fibonacci-sequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<Integer> list=new ArrayList<>();
+    public List<Integer> splitIntoFibonacci(String num) {
+        
+        if(backtrack(num,0)) return list;
+        else return new ArrayList();
+        
+    }
+    boolean backtrack(String num,int index){
+        if(index==num.length()) return list.size()>2;
+        
+        int n=0;
+        for(int i=index;i<num.length();i++){
+            n=n*10+(num.charAt(i)-'0');
+            if(n<0) return false;
+            if(list.size()<2 || list.get(list.size()-1)+list.get(list.size()-2)==n){
+                list.add(n);
+                if(backtrack(num,i+1)) return true;
+            list.remove(list.size()-1);
+            }
+            
+            if(i==index && num.charAt(i)=='0') return false;
+        }
+        return false;
+    }
+    
+}
