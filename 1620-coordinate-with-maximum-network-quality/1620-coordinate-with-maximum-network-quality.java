a8c2f43e2f1ead123379b53ffd1ab069ae43e082
Eva Albert <44654369+evialbert@users.noreply.github.com>
2022-03-22 11:32:00 +0200

    Time: 46 ms (52.00%), Space: 43.1 MB (32.00%) - LeetHub

---
+++ 1620-coordinate-with-maximum-network-quality/1620-coordinate-with-maximum-network-quality.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] bestCoordinate(int[][] towers, int radius) {
+        int res[] = new int[2];
+        double maxQuality = 0;
+        double quality = 0;
+        int finalX = 0;
+        int finalY = 0;
+        for(int i=0; i<51; i++) {
+            for(int j=0; j<51; j++) {
+                quality = 0;
+                for(int[] tower: towers) {
+                    int x = tower[0]-i;
+                    int y = tower[1]-j;
+                    double dist = Math.sqrt(x*x+y*y); 
+                    if(dist <= radius) quality += Math.floor(tower[2]/(1+dist));
+                }
+                if(maxQuality < quality) {
+                    maxQuality = quality;
+                    finalX = i;
+                    finalY = j;
+                }
+            }
+        }
+        res[0] = finalX;
+        res[1] = finalY;
+        return res;
+    }
+}
