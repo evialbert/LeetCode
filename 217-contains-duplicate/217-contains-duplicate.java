b0e62e537dcec26363e2cf448bf3e17084f60a6f
Eva Albert <44654369+evialbert@users.noreply.github.com>
2022-07-25 17:33:35 +0300

    Time: 15 ms (46.89%), Space: 68.6 MB (65.30%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
