6e2aec3e4582a618c22b70cb6cf14f561420896e
Eva Albert <44654369+evialbert@users.noreply.github.com>
2022-11-29 17:05:20 +0200

    Time: 126 ms (11.09%), Space: 27.6 MB (12.67%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int mid;
+        int l=0;
+        int h=nums.size()-1;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return -1;
+    }
+};
