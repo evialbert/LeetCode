425b65bda8b6cc0cdc5174d0c465c0ae480c481a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2022-12-01 14:45:01 +0200

    Time: 26 ms (89.67%), Space: 19.3 MB (59.07%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for(int j = 0; j < nums.size(); j++){
+            if(nums[j] != 0)
+                swap(nums[i++], nums[j]);
+        }
+    }
+};
