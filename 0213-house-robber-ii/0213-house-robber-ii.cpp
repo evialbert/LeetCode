5f4b8d6daa0b02a639a5865b732bade4c5fc5885
Eva Albert <44654369+evialbert@users.noreply.github.com>
2022-12-28 18:47:05 +0200

    Time: 0 ms (100.00%), Space: 8 MB (43.71%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int solve(vector<int> nums)
+    {
+        int n=nums.size();
+        vector<int> dp(n);
+        if(n>=1) dp[0]=nums[0];
+        if(n>=2) dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
+    }
+    
+    int rob(vector<int>& nums)
+    {
+        if(nums.size()==1) return nums[0];
+        return max(solve(vector<int>(nums.begin(),nums.end()-1)),solve(vector<int>(nums.begin()+1,nums.end())));
+    }
+};
