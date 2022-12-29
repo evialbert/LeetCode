b3cbbbada0edc6767b7a8699a0d586aa3b78e7f0
Eva Albert <44654369+evialbert@users.noreply.github.com>
2022-12-29 18:22:42 +0200

    Time: 0 ms (100.00%), Space: 6 MB (70.85%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+        int dp[m][n];
+        for(int i = m-1 ; i >= 0 ; i--){
+            for(int j = n-1 ; j >= 0 ; j--){
+                if(i == m-1 || j == n-1){
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                }   
+            }
+        }  
+        return dp[0][0];   
+    }
+};
