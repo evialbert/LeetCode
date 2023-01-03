a3a7f38f89c9e0614d55198502535a256074ff59
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-01-03 14:44:51 +0200

    Time: 4 ms (30.87%), Space: 5.9 MB (93.60%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n < 4) return n-1;
+        int ans = 1;
+        while(n >= 5){
+            n -= 3;
+            ans *= 3;
+        }
+        if(n) ans *= n;
+        return ans;
+    }
+};
