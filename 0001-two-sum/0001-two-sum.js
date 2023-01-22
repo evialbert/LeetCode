cc6100f5e3e75cb8e0a356ba932de706ae549de8
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-01-22 18:56:48 +0200

    Time: 87 ms (74.24%), Space: 43.3 MB (21.45%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let mp = new Map()
+    
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i]
+        
+        if (mp.has(diff)) {
+            return [i, mp.get(diff)]
+        }
+        
+        mp.set(nums[i], i)
+    }
+};
