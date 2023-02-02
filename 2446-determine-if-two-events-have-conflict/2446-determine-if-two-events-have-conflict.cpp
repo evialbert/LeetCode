a4891da62fe0565f1c732d497a8bd9b4e6fa6a1c
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-02-02 17:43:47 +0200

    Time: 0 ms (100.00%), Space: 11.6 MB (47.43%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) 
+    {
+        if(event1[0] < event2[0] && event1[1] >= event2[0])
+            return true;
+        else if(event1[0] > event2[0] && event1[0] <= event2[1])
+            return true;
+        else if(event1[0] == event2[0])return true; 
+
+        return false;   
+    }
+};
