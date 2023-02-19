836aa268f7a414616168020bfc5e827c695aa642
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-02-19 14:40:37 +0200

    Time: 260 ms (28.15%), Space: 8 MB (61.86%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    
-  string longestPalindrome(string A) { 
-    int n = A.size();
-    int start, length = 1;
-    for(int i=0;i<n-1;i++){
-        int l,r;
-        // for odd length 
-        l=i;r=i;
-        while(l>=0 && r<n && A[l] == A[r]){
-            if(r-l+1 > length){
-            length = r-l+1;
-            start = l;
-             }
-            l--;
-            r++;
+    string longestPalindrome(string s) {
+        int n=s.size();
+        bool dp[n][n];
+        int length=0,start;
+        for(int gap=0;gap<s.size();gap++){
+            for(int i=0,j=gap;j<s.size();i++,j++){
+                if(gap==0)
+                    dp[i][j]=true;
+                else if(gap==1){
+                    if(s[i]==s[j])
+                        dp[i][j]=true;
+                    else
+                        dp[i][j]=false;
+                }
+                else{
+                    if(s[i]==s[j] and dp[i+1][j-1]==true)
+                        dp[i][j]=true;
+                    else
+                        dp[i][j]=false;
+                }
+                if(dp[i][j]==true){
+                    length=gap+1;
+                    start=i;
+                }
+            }
         }
-         // for even length
-        l=i; r = i+1;
-        while(l>=0 && r<n && A[l] == A[r]){
-            if(r-l+1 > length){
-            length = r-l+1;
-            start = l;
-              }
-            l--;
-            r++;
-        }
-     }
-    
-    if(length == 1) return A.substr(0,1);
-     return A.substr(start,length); 
+        return s.substr(start,length);
     }
 };
