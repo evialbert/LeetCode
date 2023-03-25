0e334a8681fa6b240bdbdfb415a5f6e480c37a97
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-03-25 20:04:29 +0200

    Time: 568 ms (81.40%), Space: 184.3 MB (60.83%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,52 +1,35 @@
 class Solution {
 public:
-    int nodes;
-    vector<bool> vis;
-    
-    void dfs(vector<vector<int>> &adj, int i){
-       if(vis[i]) return;
-       
-        vis[i] = true;
-        nodes++;
-        
-        for(auto x : adj[i]){
-            dfs(adj,x);
+    int dfs(int idx,vector<vector<int>> &adj,vector<bool> &vis,int cnt)
+    {
+        vis[idx]=true;
+        int ans=cnt;
+        for(auto &child:adj[idx])
+        {
+            if(!vis[child])
+                ans+=dfs(child,adj,vis,cnt);
         }
+        return ans;
     }
-    
-    
     long long countPairs(int n, vector<vector<int>>& edges) {
+        long long ans=0;
+        vector<bool> vis(n,false);
         vector<vector<int>> adj(n);
-        vis.resize(n,false);
-        for(auto i : edges){
+        for(auto &i:edges)
+        {
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
-        
-        vector<int> v;
-        
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                dfs(adj,i);
-                v.push_back(nodes);
-                nodes = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                int k=dfs(i,adj,vis,1);
+                //cout<<i<<' '<<k<<'\n';
+                ans += 1LL*(k)*(n-k);
             }
         }
-        
-        if(v.size()<=1) return 0;
-        
-        vector<long long> prefix(v.size());
-        prefix[0] = v[0];
-        for(int i=1; i<v.size(); i++){
-            prefix[i] = prefix[i-1] + v[i];
-        }
-       
-        long long ans = 0;
-        for(int i=1; i<v.size(); i++){
-            ans += v[i]*prefix[i-1];
-        }
-        
-        return ans;
+        return ans/2;
         
     }
 };
