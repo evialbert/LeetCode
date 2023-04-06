00247ded3ff41ce9228f41831b7c0b41ce1a9e03
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-06 13:53:12 +0300

    Time: 33 ms (26.44%), Space: 36.4 MB (20.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,26 @@
 class Solution {
+vector<vector<int>>result;
 public:
-    vector<vector<int>>ans;
-    vector<int>ok;
-    void call(int pos,int n,vector<int>&v){
-        if(n==0){
-            ans.push_back(ok);
-            return ;
-        }
-        if(n<0)return;
-        for(int i=pos;i<v.size();i++)
+    void dfs(int n, int target, vector<int> &candidates, vector<int>a)
+    {
+        if(n==0)
+            return;
+        if(target==0)
+        {result.push_back(a);
+         return;}
+   
+        if(candidates[n-1]<=target)
         {
-            ok.push_back(v[i]);
-            call(i,n-v[i],v);
-            ok.pop_back();
+            a.push_back(candidates[n-1]);
+            dfs(n,target-candidates[n-1],candidates,a);
+            a.pop_back();
         }
-        return;
+        dfs(n-1,target,candidates,a);
     }
-    vector<vector<int>> combinationSum(vector<int>& v, int n) {
-        call(0,n,v);
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        vector<int>a;
+        dfs(n,target,candidates,a);
+        return result;
     }
 };
