acc4e5ca779127cfd46d2f485bf3c808dee5f650
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-07 19:40:09 +0300

    Time: 2 ms (43.61%), Space: 7 MB (8.61%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,38 @@
 class Solution {
-    private:
-    void solve(string digits, string output,int index , vector<string>&ans , string mapping[]){
-        //base case
-        if(index>=digits.length()){
-            ans.push_back(output);
-            return;
-        }
-        int number = digits[index] - '0';
-        string value = mapping[number];
-
-        for(int i = 0; i< value.length(); i++){
-            output.push_back(value[i]);
-            
-
-        solve(digits, output, index+1, ans, mapping);
-        //backtracking i.e "ad" -> "a" -> "af".....
-        output.pop_back();
-        }
-    }
 public:
-    vector<string> letterCombinations(string digits) { 
-
-        vector<string>ans;
-        if(digits.length()==0){
-            return ans;
+    vector<string> letterCombinations(string digits) {
+        map<char,vector<char>> m;
+        m['2'] = {'a','b','c'};
+        m['3'] = {'d','e','f'};
+        m['4'] = {'g','h','i'};
+        m['5'] = {'j','k','l'};
+        m['6'] = {'m','n','o'};
+        m['7'] = {'p','q','r','s'};
+        m['8'] = {'t','u','v'};
+        m['9'] = {'w','x','y','z'};
+        
+        vector<string> ans;
+        int len = digits.size();
+        
+        for(int i = 0; i<len; i++){
+            char c = digits[i];
+            vector<char> chars = m[c];
+            
+            vector<string> tmpAns;
+            vector<string> tmp = ans;
+            
+            for(auto el : chars) {
+                if(i==0){
+                    string s = "";
+                    s += el;
+                    ans.push_back(s);
+                }else {
+                    for(auto str : tmp) tmpAns.push_back(str+el);
+                    ans = tmpAns;
+                }
+            }
+            
         }
-        string output = "";
-        int index = 0;
-       string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-       solve(digits, output, index, ans, mapping);
         return ans;
     }
 };
