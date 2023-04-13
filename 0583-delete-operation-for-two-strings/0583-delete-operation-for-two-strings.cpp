9cf345c07b16db2d5af5442500ba1b4cc5668f40
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-13 17:43:35 +0300

    Time: 23 ms (64.32%), Space: 12.1 MB (59.51%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
-    int minDistance(string str1, string str2) {
-        int n=str1.size();
-	    int m=str2.size();
-	    int dp[n+1][m+1];
-	    for(int i=0; i<=n; i++)
-	    {
-	        for(int j=0; j<=m; j++)
-	        {
-	            if(j==0||i==0)
-	            dp[i][j]=0;
-	            
-	        }
-	    }
-	     for(int i=1; i<=n; i++)
-	     {
-	        for(int j=1; j<=m; j++)
-	        {
-	            if(str1[i-1]==str2[j-1])
-	            {
-	            
-	                dp[i][j]=1+dp[i-1][j-1];
-	            }
-	                else
-	            {
-	                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-	            }
-	            
-	            
-	        }
-	     }
-	     
-	     int ans=n+m-2*dp[n][m];
-	     return ans;
-	     
-    }
+	int f(int i,int j,string& w1,string& w2,vector<vector<int>>& dp){
+		if(i<0 || j<0) return 0;
+		if(dp[i][j]!=-1) return dp[i][j];
+//      Match
+		if(w1[i]==w2[j]) return dp[i][j]=1+f(i-1,j-1,w1,w2,dp);
+//      NotMatch   
+		return dp[i][j]=max(f(i-1,j,w1,w2,dp),f(i,j-1,w1,w2,dp));
+	}
+
+	int minDistance(string w1, string w2) {
+		int n=w1.size(),m=w2.size();
+		vector<vector<int>>dp(n,vector<int>(m,-1));
+		return n+m-2*f(n-1,m-1,w1,w2,dp);
+	}
 };
