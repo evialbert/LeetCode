c69024fb5c3cb3be9c6dbd807b559e03526291fb
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-15 10:24:11 +0300

    Time: 20 ms (31.36%), Space: 6.1 MB (19.23%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
-    int rangeBitwiseAnd(int left, int right) {
-        int count = 0;
-        while(left != right ){
-            left>>=1;
-            right>>=1;
-            count++;
+    int binaryToDecimal(bitset<32> num)
+    {
+        
+        int dec_value = 0;
+        bitset<1> comp(1);
+        long long base = 1;
+
+        int len = num.size();
+        for (int i = 0; i < 32; i++) {
+            if (num[i] == comp[0])
+                dec_value += base;
+            base = base * 2;
         }
-        return left<<=count;
+
+        return dec_value;
+    }
+    
+    int rangeBitwiseAnd(int left, int right) {
+        bitset<32> bset1(left);
+        bitset<32> bset2(right);
+        bitset<32> ans;
+        int i = 31;
+        while(i >= 0 && bset1[i] == bset2[i]){
+            ans[i] = bset1[i];
+            i--;
+        }
+        int a = binaryToDecimal(ans);
+        
+        return a;
     }
 };
