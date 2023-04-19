e5726ac45c1a23c22ecc35f218b8542ddfa3114a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-19 19:26:42 +0300

    Time: 3 ms (38.73%), Space: 5.9 MB (96.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> fibo = {1,1};
-        if(n<2) return fibo[n];
-        for(int i=2;i<=n;i++) fibo.push_back(fibo[i-1]+fibo[i-2]);
-        return fibo.back();
+        if (n == 1 || n == 2) return n;
+        int prev2 = 1;
+        int prev = 2;
+        int curr;
+        for (int i=3; i<=n; i++) {
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
     }
 };
