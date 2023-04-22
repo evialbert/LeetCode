80f420e470a1c619f3022c2d81850c7685706868
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-22 18:46:55 +0300

    Time: 114 ms (68.62%), Space: 70.4 MB (6.87%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        long long tempSum = 0;
-        long long maxm = nums[0];
-        for(long long x: nums){
-            tempSum = tempSum + x;
-            maxm = max(tempSum, maxm);
-            if(tempSum<0){
-                tempSum = 0;
-            }
+        vector <int> dp(nums.size(),-1);
+        dp[0]=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            dp[i]=max(nums[i]+dp[i-1],nums[i]);
+            ans=max(ans,dp[i]);
         }
-        return maxm;
+        return ans;
     }
 };
