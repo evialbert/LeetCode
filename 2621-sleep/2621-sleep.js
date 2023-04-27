06a524847a69c3cd620f79c98f733f0316476fff
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-27 21:03:49 +0300

    Time: 68 ms (13.15%), Space: 41.5 MB (87.96%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((res)=>{
+        setTimeout(()=>{
+            res()
+        }, millis)
+    })
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
