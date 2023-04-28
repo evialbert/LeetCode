6c6a17f94176677c66ef8c9708fbbc8862fce99b
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-28 18:09:28 +0300

    Time: 3 ms (51.10%), Space: 6 MB (79.53%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,28 +1,12 @@
 class Solution {
-
-    private:
-        int helper(int n,vector<int> &v)
-        {
-            if(n== 0 || n==1 )return v[n];
-            if(v[n]==-1)
-            {
-                int count{0};
-            for(int i{0};i<n;i++)
-            {
-                int left = i;
-                int right = n-1-i;
-                count+=helper(left,v) * helper(right,v);
-                v[n] = count;
-            }
-            }
-            return v[n];
-        }
-    public:
+public:
     int numTrees(int n) {
-        vector<int> v(n+1,-1);
-        v[0]= 1;
-        v[1] = 1;
-        return helper(n,v);
-        
+        int dp[n+1];
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=0;
+            for(int j=0;j<i;j++) dp[i]+=dp[j]*dp[i-j-1];
+        }
+        return dp[n];
     }
 };
