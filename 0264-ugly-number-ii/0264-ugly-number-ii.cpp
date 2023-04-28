1749ce02434e6f0b1559b91b5ff0152dd52c4338
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-28 18:06:50 +0300

    Time: 138 ms (17.13%), Space: 29.6 MB (13.94%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,15 +1,17 @@
+
 class Solution {
-public: 
+public:
     int nthUglyNumber(int n) {
-        int dp[n];
-        int a2 = 0, a3 = 0, a5 = 0;
-        dp[0] = 1;
-        for(int i = 1; i < n; i++) {
-            dp[i] = min(dp[a2] * 2, min(dp[a3] * 3, dp[a5] * 5));
-            if(dp[i] == dp[a2] * 2) a2++;
-            if(dp[i] == dp[a3] * 3) a3++;
-            if(dp[i] == dp[a5] * 5) a5++;
+        set<long> set;
+        long nthNumber = 1;
+        set.insert(nthNumber);
+        for(int i=0; i<n; i++){
+            nthNumber = *set.begin();
+            set.erase(nthNumber);
+            set.insert(nthNumber*2);
+            set.insert(nthNumber*3);
+            set.insert(nthNumber*5);
         }
-        return dp[n - 1];
+        return nthNumber;
     }
 };
