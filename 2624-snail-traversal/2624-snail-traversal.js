6da0100fd829e6236262ab096d3b53a6cebd72a2
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-28 18:15:49 +0300

    Time: 204 ms (89.80%), Space: 65.8 MB (5.10%) - LeetHub

---
+++ 2624-snail-traversal/2624-snail-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if(this.length !==  rowsCount * colsCount) return [];
+    let result = Array(rowsCount).fill([]).map(() => []);
+    for(let j = 0; j < this.length; j++){
+        const i = Math.floor(j / rowsCount);
+        if(i % 2 === 0){
+            result[j % rowsCount][i] = this[j];
+        }else{
+            result[rowsCount - j % rowsCount - 1][i] = this[j];
+        }
+    }
+    return result;
+}
+
+/**     
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */
