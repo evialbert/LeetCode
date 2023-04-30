66a0d3a483d4aecff7ccd5665981239552c04600
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-04-30 17:48:29 +0300

    Time: 4 ms (81.85%), Space: 8.9 MB (58.09%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-
-    int helper(int i, int j, vector<vector<int>>& triangle, vector<vector<int>> &dp){
-        if(i >= triangle.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int res = triangle[i][j] + min(helper(i+1,j,triangle,dp), helper(i+1,j+1,triangle,dp));
-
-        return dp[i][j] = res;
+    int find_path(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp)
+    {
+        if(i==n-1) return triangle[i][j]; //base condition: if all rows are covered then return; 
+        
+        if(dp[i][j] != -1) return dp[i][j]; //condition for using calculated path sum by dp
+        
+        //We can move in two directions bottom(i+1,j) and bottom right(i+1,j+1)
+        //so we add current path value and move in these directions 
+        int bottom = triangle[i][j] + find_path(i+1, j, n, triangle, dp); 
+        int bottom_right = triangle[i][j] + find_path(i+1, j+1, n, triangle, dp);
+        
+        //store minimum path sum in dp for further use
+        return dp[i][j] = min(bottom, bottom_right);
     }
-
-    int minimumTotal(vector<vector<int>>& triangle) {
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {   
         int n = triangle.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1,-1));
-        int ans = helper(0,0,triangle,dp);
-        return ans;
+        vector<vector<int>> dp(n, vector<int> (n,-1)); //create vector dp for storing calculated path sum
+        return find_path(0, 0, n, triangle, dp); //return minimum path sum
     }
-
 };
