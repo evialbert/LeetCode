2ae0773284225db54df372b3c042726f3bc9f737
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-05-01 09:06:40 +0300

    Time: 435 ms (22.07%), Space: 145.5 MB (12.86%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,32 +1,21 @@
 class NumMatrix {
+	vector<vector<int>> glob;
 public:
-    vector<vector<int>> arr;
-    NumMatrix(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        if(!m)return;
-        int n=matrix[0].size();
-        if(!n)return;
+	NumMatrix(vector<vector<int>>& matrix) {
+		if(matrix.size()==0 || matrix[0].size()==0) return ;
+		int m=matrix.size();
+		int n=matrix[0].size();
+		vector<vector<int>> dp(m+1,vector<int>(n+1,0));        
+		for(int i=1;i<=m;i++)
+			for(int j=1;j<=n;j++){
+				dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+matrix[i-1][j-1];
+			}
+		glob=dp;
 
-        arr = matrix;
-        for(int i=0;i<m;++i){
-            for(int j=1;j<n;++j){
-                arr[i][j] += arr[i][j-1];
-            }
-        }
-    }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=0;
-        for(int i=row1;i<=row2;++i){
-            int cur = (col1>0?(arr[i][col2] - arr[i][col1-1]): arr[i][col2]);
-            ans += cur;
-        }
-        return ans;
-    }
+	}
+
+	int sumRegion(int row1, int col1, int row2, int col2) {
+		if(glob.size()==0 || glob[0].size()==0) return 0;
+		return glob[row2+1][col2+1]-glob[row2+1][col1]-glob[row1][col2+1]+glob[row1][col1];
+	}
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
