262c844e1fcbea01bd25253c1223459b04f7a8a1
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-05-01 09:05:11 +0300

    Time: 16 ms (40.48%), Space: 9.5 MB (40.71%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int n = mat.size();
-        int m = mat[0].size();
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0) continue;
-                else if(i==0) mat[i][j] += mat[i][j-1];
-                else if(j==0) mat[i][j] += mat[i-1][j];
-                else mat[i][j] += (mat[i-1][j]+mat[i][j-1]-mat[i-1][j-1]);
+    vector<vector<int>> matrixBlockSum(vector<vector<int> >& mat, int k) {
+        int m=mat.size(),n=mat[0].size();
+        vector<vector<int> > dp(m+1,vector<int>(n+1));
+        dp[0][0]=mat[0][0];
+        for(int i=1;i<n;i++){
+            dp[0][i]=dp[0][i-1]+mat[0][i];
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0]+mat[i][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+mat[i][j];
             }
         }
-        
-        vector<vector<int>> ans(n, vector<int>(m));
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int mir = max(i-k,0);
-                int mic = max(j-k,0);
-                int mar = min(i+k,n-1);
-                int mac = min(j+k,m-1);
-                
-                if(mir==0 && mic==0) ans[i][j] = mat[mar][mac];
-                else if(mir==0) ans[i][j] = mat[mar][mac] - mat[mar][mic-1];
-                else if(mic==0) ans[i][j] = mat[mar][mac] - mat[mir-1][mac];
-                else ans[i][j] = mat[mar][mac] - mat[mar][mic-1] - mat[mir-1][mac] + mat[mir-1][mic-1];
+        vector<vector<int> > ans(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[i][j]=dp[min(i+k,m-1)][min(j+k,n-1)]-((j-k-1)>=0 ? dp[min(i+k,m-1)][j-k-1] : 0)-((i-k-1)>=0 ? dp[i-k-1][min(n-1,j+k)] : 0)+(((j-k-1)>=0 && (i-k-1)>=0) ? dp[i-k-1][j-k-1] : 0);
             }
         }
-        
         return ans;
     }
 };
