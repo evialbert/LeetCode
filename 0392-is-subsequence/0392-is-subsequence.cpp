af5de6ee40c7dcabedfdbee08f70413815f786cc
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-05-06 13:11:52 +0300

    Time: 7 ms (7.57%), Space: 6.9 MB (8.48%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) 
-    {
-        int Cntr=0;
-        if(s.size()>t.size())
-            return false;
-        
-        for(auto ch:t)
+    bool isSubsequence(string s, string t) {
+        int n = s.length(), m = t.length();
+        int dp[n+1][m+1];
+        memset(dp, 0, sizeof(dp));
+        for(int i=1;i<=n;i++)
         {
-            if(ch==s[Cntr])
-                Cntr++;
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        if(Cntr==s.size())
+        if(dp[n][m]==n)
+        {
             return true;
-        else
-            return false;
+        }
+        return false;
     }
 };
