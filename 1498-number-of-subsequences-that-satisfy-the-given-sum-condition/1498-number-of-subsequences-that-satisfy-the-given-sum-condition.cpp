b86bd8b1bb7a18a3faacb098488e65072677d0ca
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-05-06 11:04:39 +0300

    Time: 170 ms (21.73%), Space: 47.6 MB (93.20%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    int numSubseq(vector<int>A  , int target) 
-    {
-        sort(A.begin(),A.end());
-       int res = 0, n = A.size(), l = 0, r = n - 1, mod = 1e9 + 7;
-        vector<int> pows(n, 1);
-        for (int i = 1 ; i < n ; ++i)
-            pows[i] = pows[i - 1] * 2 % mod;
-         while (l <= r) {
-            if (A[l] + A[r] > target) {
-                r--;
-            } else {
-                res = (res + pows[r - l++]) % mod;
+    int mod = 1e9 + 7;
+    int power(int n){
+        //This is binary exponentiation gives 2^n because our ans in this step is nC0 + nC1 + ........ + nCn = 2^n
+        long long ans = 1,base = 2;
+        while(n){
+            if(n&1) ans = (ans*base)%mod;
+            base = (base * base) % mod;
+            n = n>>1; // n = n/2
+        }
+        return ans;
+    }
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=nums.size() - 1;
+        long long ans = 0;
+        while(i<=j){
+            if(nums[i] + nums[j] <= target){
+                ans = (ans + power(j-i)) % mod;
+                i++;
+            }
+            else {
+                if(2*nums[j] <= target) ans++;
+                j--;
             }
         }
-       
-        
-        return res;
-        
+        return ans;
     }
 };
