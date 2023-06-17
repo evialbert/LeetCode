2c91b6c148c08f864d5a9107f021a25f99d8497b
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-06-17 20:27:38 +0300

    Time: 352 ms (41.75%), Space: 96.6 MB (9.89%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
-    int mod = 1e9+7;
     int countPalindromes(string s) {
-        if(s.size()<5) return 0;
-        int n = s.size();
-        vector<vector<vector<int>>> dp1(n,vector<vector<int>>(10,vector<int>(10,0)));
-        vector<vector<vector<int>>> dp2(n,vector<vector<int>>(10,vector<int>(10,0)));
-        
-        vector<int> mp(10,0); mp[s[0]-'0']++;
-        for(int i = 1; i<s.size();++i){
-            dp1[i] = dp1[i-1];
-            for(int j = 0; j<10;++j) dp1[i][s[i]-'0'][j]+=mp[j];
-            mp[s[i]-'0']++;
+        if(s.length() < 5) return 0;
+        int M = 1e9+7;
+        vector<vector<int>> cntc(s.length(),vector<int>(10,0)); // we will store cnt of alphabets
+        cntc[0][s[0]-'0']++;
+        for(int i=1;i<s.length();i++){
+            cntc[i] = cntc[i-1];
+            cntc[i][s[i]-'0']++;
         }
-        
-        fill(mp.begin(),mp.end(),0);
-        mp[s[s.size()-1]-'0']++;
-        for(int i = s.size()-2;i>=0;i--){
-            dp2[i] = dp2[i+1];
-            for(int j = 0; j<10;++j) dp2[i][s[i]-'0'][j]+=mp[j];
-            mp[s[i]-'0']++;
+        // we have the count of chars inclutding i'th char
+        vector<vector<vector<int>>> dp(s.length(),vector<vector<int>>(10,vector<int>(10,0)));
+        vector<vector<vector<int>>> dpr(s.length(),vector<vector<int>>(10,vector<int>(10,0)));
+        for(int i=1;i<s.length();i++){
+            // for every i we have to count previous chars that is we are finding ways to make
+            // [0-9] s[i] this pair
+            dp[i] = dp[i-1];
+            for(int j=0;j<10;j++){
+                dp[i][j][s[i]-'0'] += cntc[i-1][j];
+                dp[i][j][s[i]-'0'] %= M;
+            }
         }
-        
-        long long re = 0;
-        for(int i = 2; i<s.size()-2;++i){
-            for(int a = 0; a<10;++a){
-                for(int b = 0; b<10;++b){
-                    re = (re + (long long)dp1[i-1][a][b]*dp2[i+1][a][b])%mod;
+        for(int i=s.length()-2;i>=0;i--){
+            // for every i we have to count previous chars that is we are finding ways to make
+            // [0-9] s[i] this pair
+            dpr[i] = dpr[i+1];
+            for(int j=0;j<10;j++){
+                dpr[i][s[i]-'0'][j] += (cntc[s.length()-1][j] - cntc[i][j]);
+                dpr[i][s[i]-'0'][j] %= M;
+            }
+        }
+        long int ans = 0;
+        for(int i=2;i<s.length()-2;i++){
+            // if we take i'th 
+            // we will take count of left side digits
+            for(int j=0;j<10;j++){
+                for(int k=0;k<10;k++){
+                    ans += ((1ll)*dp[i-1][j][k]*dpr[i+1][k][j]);
+                    ans %= M;
                 }
             }
         }
-        return re;
+        return ans;
     }
 };
