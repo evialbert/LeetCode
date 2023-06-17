1e5a4b3d0d5aefcb9d224a347792d6d3f6f7f869
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-06-17 20:25:50 +0300

    Time: 410 ms (37.85%), Space: 94.2 MB (50.00%) - LeetHub

--- 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
+++ 2454-next-greater-element-iv/2454-next-greater-element-iv.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     vector<int> secondGreaterElement(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> s1;  // For storing new elements
-        stack<int> s2;  // For storings elements who need 2nd grater
-        stack<int> temp; // For keeping the order intact while moving from s1 to s2
-        vector<int> res(n, -1); 
-        for(int i=0; i<n; i++){
-            // Check if current element is grater than top, then push to answer
-            while(!s2.empty() && nums[s2.top()] < nums[i]){
-                res[s2.top()] = nums[i];
-                s2.pop();
+        vector<int> res(nums.size(), -1);
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq1, pq2;
+        for (int i = 0; i < nums.size(); i++) {
+            int n = nums[i];
+            while (!pq2.empty() and pq2.top().first < n) {
+                res[pq2.top().second] = n;
+                pq2.pop();
             }
-
-            // check if we found first grater element in current
-            while(!s1.empty() && nums[s1.top()] < nums[i]){
-                temp.push(s1.top());
-                s1.pop();
+            while (!pq1.empty() and pq1.top().first < n) {
+                pq2.push(pq1.top());
+                pq1.pop();
             }
-
-            // keep the sequence intact while storing into s1
-            while(!temp.empty()){
-                s2.push(temp.top());
-                temp.pop();
-            }
-
-            // Need to find first grater element
-            s1.push(i);
+            pq1.push({n, i});
         }
+
         return res;
     }
 };
