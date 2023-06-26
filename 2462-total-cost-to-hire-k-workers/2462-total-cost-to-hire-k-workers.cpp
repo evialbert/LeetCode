0788017f4788ba8bef89556ed3fa18b649bf5b86
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-06-26 12:48:23 +0300

    Time: 204 ms (66.96%), Space: 72.8 MB (82.21%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-	long long totalCost(vector<int>& costs, int k, int candidates) {
-		priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-		int n=costs.size();
-		long long res=0;
-		int i=0,j=n-1,sz=0;
-
-		while(i<candidates) pq.push({costs[i],i++});
-
-		while(j>=i&&sz++<candidates) pq.push({costs[j],j--});
-
-		while(k-->0) {
-			if(pq.top()[1]>=j) {
-				res+=pq.top()[0];
-				pq.pop();
-				if(i<=j) pq.push({costs[j],j--});
-			}
-			else {
-				res+=pq.top()[0];
-				pq.pop();
-				if(i<=j) pq.push({costs[i],i++});
-			}
-		}
-		return res;
-	}
+    long long totalCost(vector<int>& costs, int k, int can) {
+        int n=costs.size();
+        int i=0,j=n-1;
+        priority_queue <int, vector<int>, greater<int>> fst,lst; 
+        long long int ans=0;
+        while(k>0){
+            while(fst.size()<can and i<=j){
+                fst.push(costs[i]);
+                i++;
+            }
+            while(lst.size()<can and i<=j){
+                lst.push(costs[j]);
+                j--;
+            }
+            int a,b;
+            if(fst.size()>0){
+                a=fst.top();
+            }
+            else{
+                a=INT_MAX;
+            }
+            if(lst.size()>0){
+                b=lst.top();
+            }
+            else{
+                b=INT_MAX;
+            }
+            if(b<a){
+                ans+=b;
+                lst.pop();
+            }
+            else{
+                ans+=a;
+                fst.pop();
+            }
+            k--;
+        }
+        return ans;
+    }
 };
