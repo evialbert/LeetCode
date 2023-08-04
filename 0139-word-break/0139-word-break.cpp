14111037ce68ddaae3e02835611d752b93b8dd9d
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-08-04 15:32:00 +0300

    Time: 21 ms (37.63%), Space: 13.5 MB (33.73%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,16 @@
 class Solution {
+    bool helper(string &s, set<string>& wordSet, vector<int>&dp, int pos){
+        if(pos==s.size()) return true;
+        if(dp[pos] != -1) return dp[pos];
+        for(int i=pos+1; i<=s.size(); ++i)
+            if(wordSet.count(s.substr(pos,i-pos)) and helper(s,wordSet,dp,i)) return dp[pos]=1;
+        return dp[pos]=0;
+    }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        int maxLen = 0;
-        unordered_set<string> words;
-        for (const auto& word : wordDict) {
-            words.insert(word);
-            maxLen = max(maxLen, (int)word.size());
-        }
-        vector<bool> validExcl(s.size() + 1, false);
-        validExcl[0] = true;
-        for (int i = 0; i <= s.size(); ++i) {
-            if (validExcl[i]) { // [s[0], s[i - 1]] has a valid break
-                for (int j = 1; j <= maxLen && i + j <= s.size(); ++j) {
-                    if (words.count(s.substr(i, j))) {
-                        validExcl[i + j] = true;
-                    }
-                }
-            }
-        }
-        return validExcl[s.size()];
+        set<string> wordSet;
+        for(auto a:wordDict) wordSet.insert(a);
+        vector<int>dp(s.size(),-1);
+        return helper(s, wordSet, dp, 0);
     }
 };
