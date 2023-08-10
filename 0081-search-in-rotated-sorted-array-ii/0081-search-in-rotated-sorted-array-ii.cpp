d5f48c01c40789a77667b2a2badff426294fe5c8
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-08-10 14:11:02 +0300

    Time: 7 ms (53.92%), Space: 14 MB (18.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,39 +1,11 @@
 class Solution {
 public:
-    typedef long long ll;
     bool search(vector<int>& nums, int target) {
-        ll n = nums.size();
-        return srch(nums, target, 0, n-1);
-    }
-
-    bool srch(vector<int>& nums, int target, int l, int r) {
-        ll n = nums.size();
-        while(l <= r) {
-            ll mid = (l + r)/2;
-
-            if (nums[mid] == target) return true;
-            if (nums[l] == target) return true;
-            if (nums[r] == target) return true;
-
-            if (nums[mid] == nums[l]) {
-                return srch(nums, target, l, mid-1) || srch(nums, target, mid+1, r);
-            }
-
-            if (nums[mid] >= nums[l]) {
-                if (nums[mid] >= target && nums[l] <= target) {
-                    r = mid-1;
-                } else {
-                    l = mid+1;
-                }
-            } else {
-                if (nums[mid] <= target && target <= nums[r]) {
-                    l = mid+1;
-                } else {
-                    r = mid-1;
-                }
+        for(int i=0;i<nums.size();i++) {
+            if(target == nums[i]) {
+                return true;
             }
         }
-
         return false;
     }
 };
