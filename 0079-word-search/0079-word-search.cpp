78161bff3932eaf4d93057691d6e028bd2589d71
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-08-19 15:17:19 +0300

    Time: 269 ms (94.49%), Space: 8 MB (40.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-    bool search(int i,int j,int n,int m,vector<vector<char>>& board, string word,int k){
-        if(k==word.size()) return true;
-        if(i<0||j<0||i==n||j==m||board[i][j]!=word[k]) return false;
-        char ch = board[i][j];
-        board[i][j]='#';
-        bool opt1= search(i+1,j,n,m,board,word,k+1);
-        bool opt2= search(i,j+1,n,m,board,word,k+1);
-        bool opt3= search(i-1,j,n,m,board,word,k+1);
-        bool opt4= search(i,j-1,n,m,board,word,k+1);
-        board[i][j]=ch;
-        return opt1||opt2||opt3||opt4;
-    }
     bool exist(vector<vector<char>>& board, string word) {
-        int n =board.size();
-        int m = board[0].size();
-        for(int i =0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(search(i,j,n,m,board,word,0)) return true;
+        int m = board.size(), n = board[0].size();
+        int index = 0;
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(board[i][j] == word[index])
+                {
+                    if(search(i, j, index, board, word))
+                        return true;
+                }
             }
         }
-        return false;
         
+        return false;
     }
+    
+    bool search(int i, int j, int index, vector<vector<char>>& board, string& word)
+    {
+        int m = board.size(), n = board[0].size();
+        
+        if(index == word.size()) return true;
+        if(i < 0 or j < 0 or i >= m or j >= n) return false;
+        
+        bool all = false;
+        if(word[index] == board[i][j])
+        {
+            char temp = word[index];
+            board[i][j] = '#';
+            all = search(i+1, j, index+1, board, word) or search(i-1, j, index+1, board, word) or 
+                search(i, j+1, index+1, board, word) or search(i, j-1, index+1, board, word);
+            
+            board[i][j] = temp;
+        }
+        
+        return all;
+    }
+    
 };
