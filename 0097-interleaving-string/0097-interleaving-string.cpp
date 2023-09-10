241a1ff2b14582e3bf316571d17c7d203f805c0c
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-09-10 14:44:51 +0300

    Time: 0 ms (100.00%), Space: 6.3 MB (86.81%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,30 +1,19 @@
 class Solution {
-public:
-    bool f(int i,int j, int k, string &s1, string &s2, string &s3, vector<vector<int>> &dp){
-        //base cases
-        if(i==s1.size() && j==s2.size() && k==s3.size()) return true;
-        if(i>s1.size() || j>s2.size()) return false;
-        
-        //memoization
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        //if both are same then check for both i.e. recursion
-        if(s1[i]==s3[k] && s2[j]==s3[k]){
-            return dp[i][j] = (f(i+1,j,k+1,s1,s2,s3,dp) || f(i,j+1,k+1,s1,s2,s3,dp));
-        }
-        else if(s1[i]==s3[k]){
-            return dp[i][j] = f(i+1,j,k+1,s1,s2,s3,dp);
-        }
-        else if(s2[j]==s3[k]){
-            return dp[i][j] = f(i,j+1,k+1,s1,s2,s3,dp);
-        }else{
-            return false;
-        }
+private:
+    bool solver(int i, int j, string& s1, string& s2, string& s3, int dp[][105]){
+        if(!(i+j)) return true;
+        if(dp[i][j] != -1) return false;
+        if(i && s1[i-1] == s3[i+j-1] && solver(i-1,j,s1,s2,s3,dp)) return dp[i][j] = true;
+
+        if(j && s2[j-1] == s3[i+j-1]) return dp[i][j] = solver(i,j-1,s1,s2,s3,dp);
+
+        return dp[i][j] = false;
     }
+public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n=s1.length(),m=s2.length(),len=s3.length();
-        //initialization of dp
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(0,0,0,s1,s2,s3,dp);
+        if(s1.size() + s2.size() != s3.size()) return false;
+        int dp[105][105];
+        memset(dp,-1,sizeof(dp));
+        return solver(s1.size(),s2.size(),s1,s2,s3,dp);
     }
 };
