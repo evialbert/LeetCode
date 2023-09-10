13eff0dd53cb2c0a72e420cb9939caca9ecfcc1e
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-09-10 14:34:45 +0300

    Time: 0 ms (100.00%), Space: 5.9 MB (96.89%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
+    const int M = 1000000007;
     int countOrders(int n) {
-        long long ans = 1;
-        int m = n;
-        int mod = 1e9 + 7;
-		
-		// to calculate factorial -- m*(m-1) ..... for P1 , P2 , P3
-        while(m>0){
-            ans *= m--;
-            ans %= mod;
+        long long prev = 1;
+        for(int i = 2; i<=n; i++ ){
+            prev = ((prev * (2*i - 1))%M * i)%M; 
         }
-		
-		// to calculate factorial -- (2*n-1) *(2*m - 3) ..... for D1 , D2 , D3
-        int x = 1;
-		// (2*m - x ) here refers to locations left to fill
-        while((2*n-x) > 0){
-            ans *= (2*n - x);
-            x += 2;
-            ans %= mod;
-        }
-        return ans;
+        return (int)prev%M;
     }
 };
