f2750b5b59a15147bc052dc06ab9d202acd652a0
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-09-17 15:30:38 +0300

    Time: 47 ms (57.64%), Space: 17.9 MB (45.64%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -1,53 +1,37 @@
 class Solution {
-public:
-// Class to carry three values at a time
-    class node{
-        public:
-        int val;// on which current node we are standing
-        int mask; // mask of that node
-        int cost;// cost of path explore by this node
-        node(int n,int m,int c){
-            val = n,mask = m,cost = c;
-        }
+    struct State {
+     int mask, node, dist;
     };
+public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        int n = graph.size();// total number of nodes present
-        int all = (1 << n) - 1;
-
-        queue<node> q;// queue of class node type
+        int n = graph.size();
+        int all_visited = (1 << n) - 1;
+        queue<State> q;
+        unordered_set<int> visited;
         
-        // set to take care which path we have already visited
-        set<pair<int,int>> vis;
-
-        // we don't know which node will give us shortest path so intially for all nodes we will store in our queue
-        for(int i = 0;i<n;i++){
-            node thisNode(i,1<<i,1);// make a node
-            q.push(thisNode);// push node into our queue
-            vis.insert({i,1<<i});// also them into our set
+        for (int i = 0; i < n; ++i) {
+            q.push({1 << i, i, 0});
+            visited.insert((1 << i) * 16 + i);
         }
-
-        // Implementing BFS
-        while(!q.empty()){// until queue is not empty
-            node curr = q.front();// extracting top node
-            q.pop();
+        
+        while (!q.empty()) {
+            State cur = q.front(); q.pop();
             
-            // if mask value becomes all, that means we have visited all of our nodes, so from here return cost - 1
-            if(curr.mask == all) return curr.cost - 1;
-
-            // if not, start exploring in its adjcant
-            for(auto neighbour: graph[curr.val]){
-                int newMask = curr.mask | (1<<neighbour);
+            if (cur.mask == all_visited) {
+                return cur.dist;
+            }
+            
+            for (int neighbor : graph[cur.node]) {
+                int new_mask = cur.mask | (1 << neighbor);
+                int hash_value = new_mask * 16 + neighbor;
                 
-                // if this path is not explored i.e
-                // if it is not present in our set then,
-                if(vis.find({neighbour,newMask}) == vis.end()){
-                    node newNode(neighbour,newMask,curr.cost+1);
-                    vis.insert({neighbour,newMask});
-                    q.push(newNode);
+                if (visited.find(hash_value) == visited.end()) {
+                    visited.insert(hash_value);
+                    q.push({new_mask, neighbor, cur.dist + 1});
                 }
             }
         }
-        //If no path exist then loop will eventually come out and return -1
+        
         return -1;
     }
 };
