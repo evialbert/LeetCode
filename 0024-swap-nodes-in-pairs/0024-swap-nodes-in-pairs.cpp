eafbbd4abe343f23ad1e4928daf00899902e1b74
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-09-20 18:44:44 +0300

    Time: 3 ms (44.57%), Space: 7.7 MB (7.73%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -10,12 +10,55 @@
  */
 class Solution {
 public:
+
+    void swapNode(ListNode* curr, ListNode* prev, ListNode* prev2, ListNode*&head){
+        if(prev2==NULL){
+            prev->next=curr->next;
+            curr->next=prev;
+            head=curr;
+        }
+        else{
+            prev->next=curr->next;
+            curr->next=prev;
+            prev2->next=curr;
+        }
+    }
+
+
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL || head->next==NULL)
+        ListNode* prev2=NULL;
+        ListNode* curr=NULL;
+        ListNode* prev=NULL;
+        if(head==NULL){
+            return NULL;
+        }
+        if(head and !head->next){
             return head;
-        ListNode* temp=head->next;
-        head->next=swapPairs(head->next->next);
-        temp->next=head;
-        return temp;
+        }
+        if(head and head->next){
+            curr=head->next;
+            prev=head;
+            while(curr){
+                cout<<curr->val;
+                cout<<prev->val;
+                if(prev2){
+                    cout<<prev2->val;
+                }
+                swapNode(curr, prev, prev2, head);
+                prev2=prev;
+                prev=prev->next;
+                if(curr){
+                    curr=curr->next;
+                }
+                if(curr){
+                    curr=curr->next;
+                }
+                if(curr){
+                    curr=curr->next;
+                }
+            
+            }
+        }
+        return head;
     }
 };
