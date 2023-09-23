b0ba7ffb44984ea36f34a3739e6ed48fc19dbd6a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-09-23 18:34:24 +0300

    Time: 52 ms (97.90%), Space: 17.1 MB (51.45%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,48 +1,23 @@
 class Solution {
-
-bool check(string s1, string s2){
-    if(s1.size() != s2.size()+1) return false;
-
-    int ptr1 = 0;
-    int ptr2 = 0;
-    while(ptr1 < s1.size()){
-        if(s1[ptr1] == s2[ptr2]){
-            ptr1++;
-            ptr2++;
-        }
-        else{
-            ptr1++;
-        }
-    }
-
-    if(ptr1 == s1.size() && ptr2 == s2.size()){
-        return true;
-    }
-    return false;
-}
- static bool compare(string& s1, string& s2) {
-    return s1.size() < s2.size(); 
-}
 public:
     int longestStrChain(vector<string>& words) {
-        int n = words.size();
-        int maxi = 1;
-        vector<int> dp(n,1);
-
-        sort(words.begin(), words.end(), compare);
-        
-          for(int i=1; i<n; i++){
-            for(int j =0; j<i; j++){
-               
-               if(check(words[i],words[j]) &&  1+dp[j] > dp[i]){
-                 dp[i] = dp[j] + 1;
-               }
-              
+        int count;
+        auto comp = [] (const string &a, const string &b){
+            return a.size() < b.size();
+        };
+        sort (words.begin(), words.end(),comp);
+        std::unordered_map<std::string, int> dp;
+        int max_chain = 0;
+        for (const auto& word : words) {
+            dp[word] = 1;
+            for (int i = 0; i < word.size(); ++i) {
+                std::string prev_word = word.substr(0, i) + word.substr(i + 1);
+                if (dp.find(prev_word) != dp.end()) {
+                    dp[word] = std::max(dp[word], dp[prev_word] + 1);
+                }
             }
-            if(dp[i] > maxi){
-                maxi = dp[i];
-            }
-          }
-          return maxi;
+            max_chain = std::max(max_chain, dp[word]);
+        }
+        return max_chain;
     }
 };
