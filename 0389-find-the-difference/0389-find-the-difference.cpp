edba7718502b23f1ce4fa1623361994695b041ae
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-09-25 14:51:25 +0300

    Time: 4 ms (48.11%), Space: 6.9 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result = 0;
+        for (char c : s) {
+            result ^= c; 
+        }
+        
+        for (char c : t) {
+            result ^= c; 
+        }
+        
+        return result;
+    }
+};
