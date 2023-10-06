25bfc87360d64be61eee1d5921e61a250e87d95e
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-10-06 14:02:34 +0300

    Time: 3 ms (30.50%), Space: 6.5 MB (29.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
-    int integerBreak(int n) {
-        if(n < 4) return n-1;
-        int ans = 1;
-        while(n >= 5){
-            n -= 3;
-            ans *= 3;
+    
+    int call(int n,vector<int> &dp)
+    {
+        if(n==2)
+            return 2;
+        if(dp[n]!=-1)
+            return dp[n];
+        int INT_BREK =0 , f ;
+        for(int i=n-1;i>=2;i--)
+        {
+            f = call(i,dp);
+            f *= (n-i);
+            INT_BREK = max(INT_BREK,f);
         }
-        if(n) ans *= n;
+        
+        dp[n] = max(n,INT_BREK);   
+        return INT_BREK;
+        
+            
+    }
+    
+    int integerBreak(int n) {
+        
+        if(n==2)
+            return 1;
+        vector<int> dp(n+1,-1);
+        int ans = call(n,dp);
         return ans;
+        
+        
     }
 };
