4e241a256d6e75f03894b29ca3515326f20b97b1
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-10-08 14:04:46 +0300

    Time: 0 ms (100.00%), Space: 6.2 MB (100.00%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int sum1 = 0,sum2 = 0;
+        for(int i= 1; i<=n; i++) if(i%m!=0) sum1 += i;
+        for(int i= 1; i<=n; i++) if(i%m==0) sum2 += i;
+        return sum1-sum2;
+    }
+};
