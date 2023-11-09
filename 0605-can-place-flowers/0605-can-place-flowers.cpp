4ec7695fbb0e11837dddd84b5b8a39b0338ca499
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-11-09 17:39:10 +0200

    Time: 16 ms (32.86%), Space: 20.5 MB (82.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,26 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    bool canPlaceFlowers(vector<int> &f, int n)
-    {
-        if(n==0) return true;
-        
-        f.insert(f.begin(), 0);
-        f.push_back(0);
-        auto N = static_cast<int>(f.size());
-        int i = 0;
-        int ans = 0;
-        while (i < N)
-        {
-            if (i - 1 >= 0 && f[i - 1] == 0 && f[i] == 0 && i + 1 < N && f[i + 1] == 0)
-            {
-                ++ans;
-                if (ans >= n) return true;
-                ++i;
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.size(); i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
             }
-            ++i;
         }
-
-        return false;
+        return count >= n;
     }
 };
