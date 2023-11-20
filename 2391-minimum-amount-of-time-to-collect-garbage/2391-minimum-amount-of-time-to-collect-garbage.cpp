9722365d9666ec2285a71169cd90bdbd77e35c5f
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-11-20 20:46:29 +0200

    Time: 256 ms (5.09%), Space: 102.6 MB (19.78%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& str, vector<int>& travel) {
-        int ans = 0;
-        int maxG = 0,maxP = 0,maxM = 0;
-        travel.emplace(travel.begin(),0);
-        for(int i = 1;i<travel.size();i++) travel[i] += travel[i - 1];
-        int n = str.size();
-        for(int i = 0;i<n;i++){
-            int g = 0,p = 0,m = 0;
-            for(char &c:str[i]){
-                if(c == 'G') g++;
-                else if(c == 'P') p++;
-                else m++;
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        map<char,int>pick,lastvisit;
+        int l = garbage.size();
+        for(int i=0;i<l;i++)
+        {
+            for(auto x : garbage[i])
+            {
+                pick[x]++;
             }
-            if(g) maxG = i;
-            if(p) maxP = i;
-            if(m) maxM = i;
-            ans += g + p + m;
         }
-        
-        return ans + travel[maxG] + travel[maxP] + travel[maxM];
+        for(int i=0;i<l;i++)
+        {
+            for(auto x : garbage[i])
+            {
+                lastvisit[x] = i;
+            }
+        }
+        int totaltime = 0;
+        for(auto x : pick)
+        {
+            totaltime+=x.second;
+        }
+        for(int i=0;i<travel.size()-1;i++)
+        {
+            travel[i+1]+=travel[i];
+        }
+        for(auto x : lastvisit)
+        {
+            if(x.second-1>=0)
+            {
+                totaltime+=travel[x.second-1];
+            }
+        }
+        return totaltime;
     }
 };
