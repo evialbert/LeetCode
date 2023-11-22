1ffec8d2970e9302fe77a510851c7a45d8521f81
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-11-22 16:47:37 +0200

    Time: 349 ms (8.88%), Space: 19.1 MB (21.67%) - LeetHub

---
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        def dfs(i: int, j: int) -> int:
+            if min(i, j) < 0 or max(i, j) >= len(g) or g[i][j] != 0:
+                return 0
+            g[i][j] = 1
+            return 1 + dfs(i - 1, j) + dfs(i + 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+        n, regions  = len(grid), 0
+        g = [[0] * n * 3 for i in range(n * 3)]
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == '/':
+                    g[i * 3][j * 3 + 2] = g[i * 3 + 1][j * 3 + 1] = g[i * 3 + 2][j * 3] = 1
+                elif grid[i][j] == '\\':
+                    g[i * 3][j * 3] = g[i * 3 + 1][j * 3 + 1] = g[i * 3 + 2][j * 3 + 2] = 1
+        for i in range(n * 3):
+            for j in range(n * 3):
+                regions += 1 if dfs(i, j) > 0 else 0
+        return regions
