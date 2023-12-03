7ddb693686b55a8d3d2aea279ee0ff8d4bd4bd0f
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-12-03 18:07:46 +0200

    Time: 53 ms (50.00%), Space: 16.3 MB (100.00%) - LeetHub

---
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -0,0 +1,3 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        return [i for i in range(1, len(mountain) - 1) if mountain[i - 1] < mountain[i] and mountain[i] > mountain[i + 1]]
