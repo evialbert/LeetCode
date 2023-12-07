01c6cf2f212edffc3300d4a6fb2cfd2e7269204b
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-12-07 18:01:54 +0200

    Time: 68 ms (87.35%), Space: 16.8 MB (30.42%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [1.0] + [0] * n
+        Wsum = 1.0
+        if k == 0 or n >= k + maxPts:
+            return 1
+        for i in range(1, n+1):
+            dp[i] = Wsum/maxPts
+            if i < k:
+                Wsum += dp[i]
+            if i >= maxPts:
+                Wsum -= dp[i-maxPts]
+        return sum(dp[k:])
