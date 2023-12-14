fa561b904df8d3236a9d2446e27abb835700c0f5
Eva Albert <44654369+evialbert@users.noreply.github.com>
2023-12-14 18:54:34 +0200

    Time: 976 ms (28.27%), Space: 164.2 MB (5.06%) - LeetHub

--- 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.py
+++ 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.py
@@ -1,19 +1,17 @@
 class Solution:
     def numberOfGoodPartitions(self, nums: List[int]) -> int:
-        endIndex = {}
-        for i,num in enumerate(nums):
-            endIndex[num] = i
-        groups = 0
-        rightIndex = 0
-        for i,num in enumerate(nums):
-            rightIndex = max(rightIndex,endIndex[num])
-            if rightIndex==i:
-                rightIndex = i+1
-                groups+=1
-        MOD = 10**9+7
-        ans = 1
-        
-        for i in range(groups-1):
-            ans = ((ans%MOD)*2)%MOD
-        
-        return ans
+        a={}
+        for i in range(len(nums)):
+            a[nums[i]]=i
+        def do(st):
+            if(st==len(nums)):
+                return 0
+            end=a[nums[st]]
+            p=st
+            ans=0
+            while(p<end):
+                end=max(end,a[nums[p]])
+                p+=1
+            ans+=1+do(p+1)
+            return ans
+        return (2**(do(0)-1))%1000000007
