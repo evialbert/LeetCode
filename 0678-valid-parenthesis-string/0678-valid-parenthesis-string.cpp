7ca115aec6fd8ed92c99958c7f4d01685fac53b4
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-04-07 16:56:03 +0300

    Time: 3 ms (45.50%), Space: 7.3 MB (53.16%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    bool f(int i,int n,int a,string& s,vector<vector<int>>&dp){
-        if(i==n){
-            if(a==0)return true;
-            return false;
+    bool checkValidString(string s) 
+    {
+        int leftmin=0, leftmax=0;
+        for(auto i:s)
+        {
+            if(i=='(')
+            {
+                leftmin++;
+                leftmax++;
+            }
+            else if(i=='*')
+            {
+                leftmin--;
+                leftmax++;
+            }
+            else
+            {
+                leftmin--;
+                leftmax--;
+            }
+            if(leftmax<0)
+                return false;
+            if(leftmin<0)
+                leftmin=0;
         }
-        if(dp[i][a]!=-1)return dp[i][a];
-        int ans = false;
-        if(s[i]=='('){
-            ans |= f(i+1,n,a+1,s,dp);
-        }else if(s[i]=='*'){
-            ans |= f(i+1,n,a+1,s,dp);
-            if(a) ans |= f(i+1,n,a-1,s,dp);
-            ans |= f(i+1,n,a,s,dp);
-        }else {
-            if(a) ans |= f(i+1,n,a-1,s,dp);
-        }
-        return dp[i][a] = ans;
-    }
-    bool checkValidString(string s) {
-        vector<vector<int>>dp(s.size(),vector<int>(101,-1));
-        return f(0,s.size(),0,s,dp);
+        return (leftmin==0)? true : false;
     }
 };
