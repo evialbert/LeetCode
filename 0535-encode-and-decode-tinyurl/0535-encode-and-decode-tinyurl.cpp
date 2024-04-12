32d73d572240796429494a689ae5189686ca2e85
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-04-12 18:05:25 +0300

    Time: 0 ms (100.00%), Space: 8.2 MB (84.89%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+	string encode(string longUrl) {
+	   return longUrl; 
+	}
+	string decode(string shortUrl) {
+		return shortUrl; 
+	}
+};
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
