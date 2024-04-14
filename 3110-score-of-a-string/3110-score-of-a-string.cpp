821146d33d96e196923d25ca77ee8490fab34e21
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-04-14 21:38:50 +0300

    Time: 0 ms (100.00%), Space: 7.7 MB (80.00%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,11 @@
+// Time complexity -> O(n) and Space -> O(1)
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum=0;
+        for(int i=0;i<s.size()-1;i++){
+            sum+=abs(s[i]-s[i+1]);
+        }
+        return sum;
+    }
+};
