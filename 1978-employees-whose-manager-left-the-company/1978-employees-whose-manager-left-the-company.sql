684b997b47a12bb2ee03bd908b77b47f660db91e
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-05-11 17:40:36 +0300

    Time: 808 ms (87.40%), Space: 0B (100.00%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+select employee_id from employees where manager_id not in (
+select employee_id from employees) and salary <30000
+order by employee_id 
