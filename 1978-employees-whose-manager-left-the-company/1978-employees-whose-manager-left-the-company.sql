62dffe0be5b39a1eb450fd5cbbec3960b7578454
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-05-11 17:46:10 +0300

    Time: 1154 ms (32.92%), Space: 0B (100.00%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,8 +1,17 @@
-/* Write your PL/SQL query statement below */
-SELECT employee_id
-FROM 
-Employees
-WHERE 
-salary < 30000
-and manager_id not in (select employee_id from Employees)
-ORDER BY employee_id
+/* Write your T-SQL query statement below */
+select
+	e.employee_id
+from Employees as e
+where e.salary < 30000
+and exists
+	(
+		select
+			e2.manager_id
+		from Employees as e1
+		right outer join Employees as e2
+		on e1.employee_id = e2.manager_id
+		where e1.employee_id is null
+		and e2.manager_id is not null
+		and e.employee_id = e2.employee_id
+	)
+order by e.employee_id;
