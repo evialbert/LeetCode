a8e4196b0d67a6e5e18268e38a28c45f3ac26910
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-06-24 13:36:24 +0300

    Time: 88 ms (68.44%), Space: 113.2 MB (34.84%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int minKBitFlips(vector<int>& A, int K) {
-        int cur = 0, res = 0, n = A.size();
-        for (int i = 0; i < n; ++i) {
-            if (i >= K && A[i - K] > 1) {
-                cur--;
-                A[i - K] -= 2;
+    int minKBitFlips(vector<int>& nums, int k) {
+        int ans = 0;
+
+        int i = 0, j = 0;
+        int n = nums.size();
+        vector<int> reset(n,0);
+        
+        int flag = 1;
+        while(i<=(n-k)){
+            flag = flag^reset[i];
+            if(nums[i]^flag){
+                ans++;
+                if(i+k<n){
+                    reset[i+k] = 1;
+                }
+                flag = !flag;
             }
-            if (cur % 2 == A[i]) {
-                if (i + K > n) return -1;
-                A[i] += 2;
-                cur++;
-                res++;
-            }
+            i++;
         }
-        return res;
+
+        while(i<n){
+            flag = flag^reset[i];
+            if(nums[i]^flag){
+                return -1;
+            }
+            i++;
+        }
+
+        return ans;
     }
 };
