d06093089def1af15c5f967c7c136b7ec9ecbe3e
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-06-26 15:18:31 +0300

    Time: 66 ms (93.50%), Space: 46.5 MB (85.20%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -10,40 +10,30 @@
  * };
  */
 class Solution {
+    vector<TreeNode*> sorted;
 public:
-    void inorder(TreeNode* root,vector<int>& in){
-        if(root == NULL) return;
-        inorder(root->left,in);
-        in.push_back(root->val);
-        inorder(root->right,in);
+    void inorder(TreeNode* root) {
+        if (root == NULL) 
+            return;
+        
+        inorder(root->left);
+        sorted.push_back(root);
+        inorder(root->right);
     }
-    void make(vector<int> in,TreeNode* &root,TreeNode* r,int s,int e,string str){
-        if(s>e) return;
-        int m = s + (e-s)/2;
-        TreeNode* node = new TreeNode(in[m]);
-        if(str == ""){
-            root = node;
-        }
-        else if(str == "R"){
-            r->right = node;
-        }
-        else if(str == "L"){
-            r->left = node;
-        }
-
-        if(s == e) return;
-
-        make(in,root,node,s,m-1,"L");
-        make(in,root,node,m+1,e,"R");
+    
+    TreeNode* createTree(int start, int end) {
+        if (start > end) 
+            return NULL;
+        
+        int mid = start + (end - start)/2;
+        TreeNode *root = sorted[mid];
+        root->left = createTree(start, mid - 1);
+        root->right = createTree(mid + 1, end);
+        return root;
+        
     }
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> in;
-        inorder(root,in);
-
-        TreeNode* nroot = NULL;
-        TreeNode* r = NULL;
-        string s = "";
-        make(in,nroot,r,0,in.size()-1,"");
-        return nroot;
+        inorder(root);
+        return createTree(0, sorted.size()-1);
     }
 };
