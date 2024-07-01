3c265d258772dd6b467987e986048fcebaa9e18b
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-07-01 17:46:03 +0300

    Time: 3 ms (60.88%), Space: 10.5 MB (45.76%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        if(arr.size()<3)return false;
+        for (int i = 0; i <= arr.size() - 3; ++i) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
