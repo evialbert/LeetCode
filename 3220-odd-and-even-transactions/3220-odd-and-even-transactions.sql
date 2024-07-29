f10de7b1502610d583369bb13579836c5e207141
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-07-29 19:42:58 +0300

    Time: 299 ms (52.69%), Space: 0B (100.00%) - LeetHub

--- 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -1,9 +1,6 @@
-# Write your MySQL query statement below
-SELECT 
-    transaction_date,
-    SUM(CASE WHEN amount % 2 !=0 THEN amount ELSE 0 END) AS odd_sum,
-    SUM(CASE WHEN amount % 2 = 0 THEN amount ELSE 0 END) AS even_sum
-FROM
-    transactions
-GROUP BY transaction_date
-ORDER BY 1 ASC
+/* Write your T-SQL query statement below */
+select   transaction_date
+        ,sum(case when isnull(amount, 0) % 2 = 1 then isnull(amount, 0) else 0 end) as odd_sum
+        ,sum(case when isnull(amount, 0) % 2 = 0 then isnull(amount, 0) else 0 end) as even_sum
+from transactions
+group by transaction_date
