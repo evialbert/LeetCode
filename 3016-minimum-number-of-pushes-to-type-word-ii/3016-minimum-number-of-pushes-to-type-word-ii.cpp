e7280ddb9bba91b60608e490c7bc86a21e0e88ad
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-08-06 13:32:53 +0300

    Time: 66 ms (87.01%), Space: 25.1 MB (57.94%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int minimumPushes(string s) {
-        int n =(int)s.size() ; 
-        vector <int> v(26,0) ;  
-        for(int i = 0 ; i < n ; i++){
-             v[s[i] - 'a']++ ; 
+    int minimumPushes(string word) {
+        vector<int> freq(26,0);
+        for(int i=0;i<word.size();i++){
+            freq[word[i]-'a']++;
         }
-        sort(v.begin() , v.end()) ;
-        reverse(v.begin() , v.end()) ; 
-        int ans = 0 ; 
-        for(int i = 0 ; i < (int)v.size() ;i++){
-            if(i < 8){
-                ans += v[i]*1 ; 
-            }
-            else if(i >= 8 && i < 16){
-                ans += v[i]*2 ; 
-            }
-            else if(i >= 16 && i < 24){
-                ans += v[i]*3 ; 
+        sort(freq.begin(),freq.end(),greater<int>());
+        int curr=1,count=0,ans=0;
+        for(int i=0;i<freq.size()&&freq[i]>0;i++){
+            if(count!=8){
+                ans+=(freq[i]*curr);
+                count++;
             }
             else{
-                ans += v[i]*4 ; 
+                count=0;
+                curr++;
+                ans+=(freq[i]*curr);
+                count++;
             }
         }
-        return ans ; 
+        return ans;
     }
 };
