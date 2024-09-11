8f04bc54338de78325e59664397782abd6269b94
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-09-11 18:18:03 +0300

    Time: 0 ms (100.00%), Space: 40.5 MB (35.00%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int count = 0;
+        while (xor != 0) {
+            count += xor & 1;
+            xor >>= 1;
+        }
+        return count;
+    }
+}
