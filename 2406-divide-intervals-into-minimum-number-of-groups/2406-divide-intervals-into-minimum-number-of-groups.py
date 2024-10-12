b1bcdbfbf6d18ec3660778417c86188dd8e780fa
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-10-12 14:02:23 +0300

    Time: 1003 ms (97.26%), Space: 55.8 MB (93.67%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,17 +1,15 @@
-class Solution {
-    public int minGroups(int[][] intervals) {
-        int[] cnt = new int[1_000_002];
-        for(var i:intervals){
-            cnt[i[0]]++;
-            cnt[i[1]+1]--;
-        }
-
-
-        int maxi=0, temp=0;
-        for(int c:cnt){
-            temp += c;
-            maxi = Math.max(maxi, temp);
-        }
-        return maxi;
-    }
-}
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        n = len(intervals)
+        s, e = [intervals[i][0] for i in range(n)], [intervals[i][1] for i in range(n)]
+        s.sort()
+        e.sort()
+        output = 0
+        s_i, e_i = 0, 0
+        while s_i < n:
+            if s[s_i] <= e[e_i]:
+                s_i += 1
+            else:
+                e_i += 1
+            output = max(output, s_i - e_i)
+        return output
