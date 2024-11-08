d7d16e2b61149dd430f947a99a20eb94e87adeb5
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-11-08 17:32:42 +0200

    Time: 157 ms (26.37%), Space: 13.8 MB (92.31%) - LeetHub

--- 0638-shopping-offers/0638-shopping-offers.cpp
+++ 0638-shopping-offers/0638-shopping-offers.cpp
@@ -1,52 +1,45 @@
 class Solution {
-    int mini;
+private:
+    int n, m;
+    int solve(vector<int>& price, vector<vector<int>>& special, 
+        vector<int>& needs, int i) {
+        // base case
+        if(i == m) {
+            int ret = 0;
+            for(int i = 0; i < n; i++) {
+                ret += needs[i] * price[i];
+            }
+            return ret;
+        }
+        // calling recursion
+        bool flag = true;
+        for(int j = 0; j < n; j++) {
+            if(needs[j] < special[i][j]) {
+                flag = false;
+                break;
+            }
+        }
+        int op1 = INT_MAX;
+        if(flag) {
+            // if can take curr offer
+            for(int j = 0; j < n; j++) {
+                needs[j] -= special[i][j];
+            }
+            op1 = special[i][n] + solve(price, special, needs, i);
+            for(int j = 0; j < n; j++) {
+                needs[j] += special[i][j];
+            }
+        }
+        int op2 = solve(price, special, needs, i + 1);
+        return min(op2, op1);
+    }
 public:
-    void calc(vector<int>& price, vector<vector<int>>& special, vector<int>& needs,int n,int c)
-    {
-        if(n==0)
-        {
-            mini=min(c,mini);
-            return;
-        }
-        if(c>=mini)
-            return;
-        for(int i=0;i<special.size();++i)
-        {
-            bool check=true;
-            int s=0;
-            for(int j=0;j<needs.size();++j)
-            {
-                if(needs[j]<special[i][j])
-                {
-                    check=false;
-                }
-                if(!check)
-                    break;
-            }
-            if(check)
-            {
-                for(int j=0;j<needs.size();++j)
-                {
-                    s+=special[i][j];
-                    needs[j]-=special[i][j];
-                }
-                calc(price,special,needs,n-s,c+special[i].back());
-                for(int j=0;j<needs.size();++j)
-                    needs[j]+=special[i][j];
-            }
-                
-            }
-        for(int i=0;i<price.size();++i)
-            c+=(needs[i]*price[i]);
-        mini=min(c,mini);
-         return;
-        }
-    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
-        int n=0;
-        for(int i=0;i<needs.size();++i)
-            n+=needs[i];
-        mini=INT_MAX;
-         calc(price,special,needs,n,0);
-        return mini;
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, 
+        vector<int>& needs) {
+        // using backtracking
+        n = price.size();
+        m = special.size();
+        int ans = solve(price, special, needs, 0);
+        return ans;
     }
 };
