51008d8ac12f57aec5d640fa8877f42b1d1603bb
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-11-11 18:03:47 +0200

    Time: 3 ms (100.00%), Space: 52.6 MB (71.43%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.js
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.js
@@ -3,34 +3,35 @@
  * @return {boolean}
  */
 var primeSubOperation = function(nums) {
-    const pick = (num, r) => {
-        let p = Math.max(2, num - r + 1), d
-        if (p >= num)
-            return 0
-        if (p == 2)
-            return 2
-        if (p % 2 == 0)
-            p += 1
-        while (p < num) {
-            d = 3
-            while (p % d) {
-                if (p < d * d)
-                    return p
-                d += 2
-            }
-            p += 2
+    const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+        41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
+        103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 
+        163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 
+        227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 
+        281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 
+        353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 
+        421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 
+        487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 
+        569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 
+        631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 
+        701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 
+        773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 
+        857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 
+        937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
+
+    let last = nums.at(-1);
+    for(let i = nums.length - 2; i >= 0; i--) {
+        if(nums[i] < last) {
+            last = nums[i];
+            continue;
         }
-        return 0
+
+        const dif = nums[i] - last;
+        const minSubPrime = primes.find((x) => x > dif);
+        if(!minSubPrime || minSubPrime >= nums[i]) {
+            return false;
+        }
+        last = nums[i] - minSubPrime;
     }
-    let d = nums.at(-1)
-    for (let i = nums.length-2; i >= 0; i--) {
-        if (nums[i] >= d) {
-            p = pick(nums[i], d)
-            if (p == 0)
-                return false
-            d = nums[i] - p
-        } else 
-            d = nums[i]
-    }
-    return true
+    return true;
 };
