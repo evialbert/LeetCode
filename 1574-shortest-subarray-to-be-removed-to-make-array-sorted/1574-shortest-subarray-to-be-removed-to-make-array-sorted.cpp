ca8546a48051999096e4c8c5cc4d11f8615aba46
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-11-15 13:17:44 +0200

    Time: 0 ms (100.00%), Space: 69.4 MB (85.81%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    int findLengthOfShortestSubarray(vector<int>& arr) {
-        int n = (int)arr.size();
-
-        int s = 0, e = n-1;
-        while(s < n-1 && arr[s] <= arr[s+1]) s++;
-        if(s == n-1) return 0;
-
-        while(e >= s && arr[e] >= arr[e-1]) e--;
-        if(e == 0) return n-1;
-
-        int ans = min(n-1-s,e);
-        int i = 0, j = e;
-        while(i <= s && j < n)
-        {
-            if(arr[j] >= arr[i])
-            {
-                ans = min(ans,j-i-1);
-                i++;
+    int findLengthOfShortestSubarray(vector<int>& a) {
+        int n=a.size();
+        if(n<=1) return 0;
+        // Let's first calculate the longest increasing prefix and longest increasing suffix.
+        int l,r;
+        for(l=1;l<n;++l){
+            if(a[l] < a[l-1]){
+                break;
             }
-            else j++;
+        }
+        for(r=n-1;r>=1;r--){
+            if(a[r] < a[r-1]) {
+                break;
+            }
         }
 
-        return ans;
+        // overalpping
+        l--;
+        if(l>=r) return 0;
+
+        /* 
+        To calculate the minimum we have to calculate for each prefix what can be the suffix we can include and then calculate
+        the length of the shortest subarray to be removed.
+
+        Let's say there are two pointers i=l+1,j=r-1 which represents the current subarray to be removed. Check for each
+        value of i and j, meaning each prefix and suffix and then calculate the valid subarray to removed and then calculate the ans.
+        Takes n2 so use two pointers as both the left and right parts are sorted. or binary search to make it logn instead of n.
+        */
+
+        int mini = min(n-l-1,r);
+        int i=0,j=r;
+        while(i<=l and j<n){
+            if(a[j] >= a[i]) mini=min(mini,j-i-1),i++;
+            else j++;
+        }
+        return mini;
     }
 };
