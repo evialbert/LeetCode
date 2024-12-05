2fd1aac90f65e6977c24d85b6359608dbdd13ca1
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-12-05 08:06:37 +0200

    Time: 7 ms (77.01%), Space: 22 MB (33.33%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        if (start == target) {
+            return true;
+        }
+        int waitL = 0;
+        int waitR = 0;
+        
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start[i];
+            char goal = target[i];
+            if (curr == 'R') {
+                if (waitL > 0) {
+                    return false;
+                }
+                waitR++;
+            }
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
+        }
+        return waitL == 0 && waitR == 0;
+    }
+};
