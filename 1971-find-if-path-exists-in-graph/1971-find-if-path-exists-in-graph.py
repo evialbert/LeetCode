ecc6083f627546d91923f16582b909c8fa9187df
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-12-14 15:06:25 +0200

    Time: 324 ms (81.35%), Space: 82.7 MB (94.78%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,35 +1,37 @@
-class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if source == destination:
-            return True
-        
-        adjMap = self.buildAdjMap(edges)
-        
-        if source not in adjMap or destination not in adjMap:
-            return False
-        
-        return self.BFS(n, source, destination, adjMap)
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        # Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+        # The initial "rank" of each vertex is 1, because each of them is
+        # a standalone vertex with no connection to other vertices.
+        self.rank = [1] * size
+
+    # The find function here is the same as that in the disjoint set with path compression.
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
     
-    def buildAdjMap(self, edges):
-        adjMap = collections.defaultdict(list)
-        for edge in edges:
-            u, v = edge
-            adjMap[u].append(v)
-            adjMap[v].append(u)
-        return adjMap
-        
-    def BFS(self, n, source, destination, adjMap):
-        queue = collections.deque([source])
-        visited = [0] * n
-        visited[source] = 1
-        while queue:
-            vertex = queue.popleft()
-            for neighbor in adjMap[vertex]:
-                if neighbor == destination:
-                    return True
-                
-                if visited[neighbor] == 0:
-                    visited[neighbor] = 1
-                    queue.append(neighbor)
-                    
-        return False
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        uf = UnionFind(n)
+        for u, v in edges: 
+            uf.union(u, v)
+        return uf.connected(start, end)
