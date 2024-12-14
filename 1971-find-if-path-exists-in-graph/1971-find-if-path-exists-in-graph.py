1722374da75c3bc5988c1cd7a840776b723705b1
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-12-14 15:06:38 +0200

    Time: 384 ms (63.23%), Space: 96.1 MB (62.76%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,37 +1,17 @@
-class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-        # Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
-        # The initial "rank" of each vertex is 1, because each of them is
-        # a standalone vertex with no connection to other vertices.
-        self.rank = [1] * size
-
-    # The find function here is the same as that in the disjoint set with path compression.
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-
-    # The union function with union by rank
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
-
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-    
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
-        uf = UnionFind(n)
+        graph = {}
         for u, v in edges: 
-            uf.union(u, v)
-        return uf.connected(start, end)
+            graph.setdefault(u, []).append(v)
+            graph.setdefault(v, []).append(u)
+        
+        seen = {start}
+        stack = [start]
+        while stack: 
+            n = stack.pop()
+            if n == end: return True 
+            for nn in graph.get(n, []): 
+                if nn not in seen: 
+                    seen.add(nn)
+                    stack.append(nn)
+        return False 
