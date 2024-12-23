36c096f2a255291aa19ea8c459ceb6e33d1b9503
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-12-23 18:19:35 +0200

    Time: 0 ms (100.00%), Space: 18.1 MB (6.09%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,41 +1,23 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        
-        map = defaultdict(list)
-        eqns = {}
-
-        for i,j in equations:
-           map[i].append(j)
-           map[j].append(i)
-
-        for i in range(len(equations)):
-            eqns[(equations[i][0], equations[i][1])] = values[i]
-        
-        
-        def connected( node, target, ans):
-            if node in visited:
-                return False
-            visited.add(node)
-            if target in map[node]:
-                if (node, target) in eqns:
-                    val = ans * eqns[(node, target)]
-                else:
-                    val = ans / eqns[(target, node)]
-                res.append(val)
-                return True
+        # directed graph as adjacency list 
+        digraph = {} 
+        for (u, v), w in zip(equations, values): 
+            digraph.setdefault(u, []).append((v, w))
+            digraph.setdefault(v, []).append((u, 1/w))
             
-            for nei in map[node]:
-                if (node, nei) in eqns:
-                    val = ans * eqns[(node, nei)]
-                else:
-                    val = ans / eqns[(nei, node)]
-                if connected(nei, target, val):
-                    return True
-        res = []
-        for i,j in queries:
-            visited = set()
-            if not connected(i, j, 1):
-                res.append(-1.0)
-
-        return res
-            
+        # query 
+        def dfs(u, v, w=1): 
+            """Return division via dfs."""
+            if u not in digraph: return -1 
+            if u == v: return w
+            seen.add(u)
+            for uu, ww in digraph.get(u, []): 
+                if uu not in seen and (ans := dfs(uu, v, w*ww)) != -1: return ans 
+            return -1
+        
+        ans = []
+        for u, v in queries: 
+            seen = set()
+            ans.append(dfs(u, v))
+        return ans 
