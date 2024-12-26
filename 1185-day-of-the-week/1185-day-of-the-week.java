58260c9bed2c42952c3ba1a912fb682570fab516
Eva Albert <44654369+evialbert@users.noreply.github.com>
2024-12-26 13:41:36 +0200

    Time: 0 ms (100.00%), Space: 41 MB (42.30%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.java
+++ 1185-day-of-the-week/1185-day-of-the-week.java
@@ -1,13 +1,35 @@
-class Solution {
-public:
-    const string answers[7]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    const int days[12]{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    string dayOfTheWeek(int day, int month, int year) {
-        long long d = day; 
-        for (int i = 1968; i < year; i++) // 168.01.01 - monday
-            d += (abs(i - 2000) % 4 == 0) ? 366 : 365;
-        d += (abs(year - 2000) % 4 == 0 and year != 2100 and month > 2); // 2000 - leap, 2100 - not
-        d += accumulate(begin(days), begin(days) + month - 1, 0);
-        return answers[d % 7];
+class Solution {   
+public String dayOfTheWeek(int day, int month, int year) {
+        String[] week = { "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday" };
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= 12; i++) {
+            if (i == 2)
+                map.put(i, 28);
+            else if (i == 4 || i == 6 || i == 9 || i == 11)
+                map.put(i, 30);
+            else
+                map.put(i, 31);
+        }
+
+        int count = 0;
+        for (int i = 1971; i < year; i++) {
+            if (i % 4 == 0 && ((i % 100 != 0) || (i % 400 == 0)))
+                count += 366;
+            else
+                count += 365;
+        }
+
+        if (year % 4 == 0 && ((year % 100 != 0) || (year % 400 == 0)))
+            map.put(2, 29);
+
+        int total = 0;
+        for (int i = 1; i < month; i++)
+            total += map.get(i);
+
+        int totalTillTheDate = count + total + day;
+
+        int rem = (totalTillTheDate) % 7;
+
+        return week[rem];
     }
-};
+}
