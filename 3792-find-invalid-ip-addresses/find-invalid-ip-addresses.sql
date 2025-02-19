90e9b5884e9bf66f31fb92af243fdec53d9a223b
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-02-19 18:43:51 +0200

    Time: 475 ms (62.50%) | Memory: 0B (100.00%) - LeetSync

--- 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
+++ 3792-find-invalid-ip-addresses/find-invalid-ip-addresses.sql
@@ -1,16 +1,31 @@
-# Write your MySQL query statement below
-SELECT
-  ip,
-  COUNT(log_id) AS invalid_count
-FROM
-  logs
-WHERE
-  NOT regexp_like(
+WITH ip_octets AS (
+    SELECT l.log_id,
+    l.ip,
+    s.value AS ip_parts
+    FROM logs l
+    CROSS APPLY STRING_SPLIT(l.ip , '.') s
+),
+validity_ip AS (
+    SELECT log_id,
     ip,
-    "^(?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:[.](?:[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
-  )
-GROUP BY
-  ip
-ORDER BY
-  invalid_count DESC,
-  ip DESC;
+    CASE WHEN ip_parts LIKE '0%' OR
+    (COUNT(ip_parts) OVER (partition by log_id ORDER BY ip ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)) !=4 OR 
+    CAST(ip_parts AS INT) > 255 then 'invalid' else 'valid' end AS validity_check
+    FROM ip_octets  
+),
+invalid_ip_distinct AS (
+    SELECT distinct log_id,
+    ip
+    FROM validity_ip
+    WHERE validity_check = 'invalid'
+)
+SELECT ip,
+    COUNT(*) AS invalid_count
+FROM invalid_ip_distinct
+GROUP BY ip
+ORDER BY invalid_count desc,ip desc
+
+
+
+
+
