d0e500c9c155847857e2feada130460cfde462c2
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-02-25 21:04:11 +0200

    Time: 439 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 3803-find-products-with-valid-serial-numbers/find-products-with-valid-serial-numbers.sql
+++ 3803-find-products-with-valid-serial-numbers/find-products-with-valid-serial-numbers.sql
@@ -1,2 +1,13 @@
-# Write your MySQL query statement below
-select * from products where description like '% SN____-____' or description like '% SN____-____ %'
+/* Write your T-SQL query statement below */
+select
+ * 
+from 
+products 
+where 
+description like '%SN[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]%'
+and 
+(
+substring(description,charindex('-',description)+5,1)=' ' 
+or 
+charindex('-',description)+4 = len(description)
+)
