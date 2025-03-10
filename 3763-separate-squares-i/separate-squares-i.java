6d23da247e3614faf9802192c55da4458e7f7b80
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-03-10 15:05:09 +0200

    Time: 192 ms (29.24%) | Memory: 86.5 MB (62.72%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.java
+++ 3763-separate-squares-i/separate-squares-i.java
@@ -1,46 +1,50 @@
 class Solution {
-    private static double area(int[][] squares, double y) {
-
-        // Compute only one side of area and return it 
+    public static double func(double mid, int[][] squares) {
         double area = 0;
-        for (int[] s : squares) {
-            int x = s[0], yb = s[1], l = s[2];
-            int t = yb + l;
-            if (y >= t) {
-                continue;
-            } else if (y <= yb) {
-                area += (double)l * l;
-            } else {
-                area += (double)(t - y) * l;
+        for (int[] sq : squares) {
+            double y = sq[1];
+            double l = sq[2];
+              if (mid >= y + l) continue; 
+
+            if (mid <= y) { 
+                area += l * l;
+            } else if (mid-y <l) { 
+                area += (y + l - mid) * l;
             }
         }
+      
         return area;
     }
-
-    public double separateSquares(int[][] squares) {
-        int min = Integer.MAX_VALUE;
-        int max = 0;
-        for (int i = 0; i < squares.length; i++) {
-            min = Math.min(min, squares[i][1]);
-            max = Math.max(max, squares[i][1] + squares[i][2]);
-        }
-
-        double low = min, high = max;
-        double area = 0;
-        for (int[] s : squares) {
-            area += (double)s[2] * s[2];
-        }
-        area /= 2.0;
-
-        while (low + 1e-6 < high) {
+    
+    public static double binary_search(double low, double high, double target, int[][] squares) {
+        for (int i = 0; i < 100; i++) {
             double mid = (low + high) / 2.0;
-            if (area(squares, mid) > area) {
+            double area = func(mid, squares);
+            if (area > target) {
                 low = mid;
             } else {
                 high = mid;
             }
         }
-
-        return Math.round(low * 1e5) / 1e5;
+        return high;
+    }
+  
+    public double separateSquares(int[][] squares) {
+        double ta = 0;
+        double min_y = Double.MAX_VALUE;
+        double max_y = -Double.MAX_VALUE;
+        for (int[] square : squares) {
+            double l = square[2];
+            ta += l * l;
+        }
+          for (int[] square : squares) {
+            double l = square[2];
+            double y = square[1];
+            min_y = Math.min(min_y, y);
+            max_y = Math.max(max_y, y + l);
+        }
+  
+        double ha = ta / 2.0;
+        return binary_search(min_y, max_y, ha, squares);
     }
 }
