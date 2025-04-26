86f2d21ea0a43bb0a289b9b1e9c0c9cf1a3e893a
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-04-26 15:10:28 +0300

    Time: 2 ms (45.53%) | Memory: 55.1 MB (46.57%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    return [...set1].filter((num)=>set2.has(num));
+};
