327f1f5e30fbbbd16c33325442898ed320328676
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-05-01 13:34:28 +0300

    Time: 693 ms (67.83%) | Memory: 0B (100.00%) - LeetSync

--- 3865-find-product-recommendation-pairs/find-product-recommendation-pairs.sql
+++ 3865-find-product-recommendation-pairs/find-product-recommendation-pairs.sql
@@ -1,11 +1,36 @@
-# Write your MySQL query statement below
-SELECT a.product_id AS product1_id,b.product_id AS product2_id,c.category AS product1_category,d.category AS product2_category,COUNT(*) AS customer_count FROM ProductPurchases a
-JOIN ProductPurchases b
-ON a.user_id=b.user_id AND a.product_id<b.product_id
-JOIN ProductInfo c
-ON a.product_id=c.product_id
-JOIN ProductInfo d
-ON b.product_id=d.product_id
-GROUP BY a.product_id,b.product_id
-HAVING COUNT(*)>=3
-ORDER BY customer_count DESC,product1_id ASC,product2_id ASC;
+/* Write your T-SQL query statement below */
+WITH unique_purchases AS (
+    SELECT a.user_id, a.product_id, b.category
+    FROM ProductPurchases a
+    INNER JOIN ProductInfo b ON a.product_id = b.product_id
+),
+pair AS (
+    SELECT  
+        a.user_id, 
+        a.product_id AS product1_id,
+        a.category AS product1_category,
+        b.product_id AS product2_id  ,
+        b.category AS product2_category
+    FROM unique_purchases a
+    JOIN unique_purchases b
+        ON a.user_id = b.user_id AND a.product_id < b.product_id
+)
+
+SELECT 
+    p.product1_id,
+    p.product2_id,
+    p.product1_category,
+    p.product2_category,
+    COUNT(*) AS customer_count
+FROM pair p
+GROUP BY 
+    p.product1_id, 
+    p.product2_id, 
+    p.product1_category, 
+    p.product2_category
+HAVING
+    COUNT(*) > 2
+ORDER BY
+    customer_count DESC,
+    p.product1_id, 
+    p.product2_id;
