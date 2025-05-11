4cb3d4ec50a367c97fb1fd0436ecb8e89eefb37c
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-05-11 13:57:12 +0300

    Time: 0 ms (100.00%) | Memory: 55.4 MB (13.53%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    let count = 0;
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]%2 !==0){
+        count++;
+      }else {
+        count = 0;
+      }
+      if(count == 3) return true;
+    }
+    return false;
+};
