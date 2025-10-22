5d5c3cc39324cf5d701e5b08f01f3fed06c6b122
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-10-22 19:17:39 +0300

    Time: 1 ms (100.00%) | Memory: 40.8 MB (87.65%) - LeetSync

---
+++ 4023-count-binary-palindromic-numbers/count-binary-palindromic-numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    private long makePalin(long left, boolean odd) {
+        long ans = left;
+        if (odd)
+            left = left >> 1;
+        while (left > 0) {
+            ans = (ans << 1) | (left & 1);
+            left = left >> 1;
+        }
+        return ans;
+    }
+
+    public int countBinaryPalindromes(long n) {
+        if (n == 0)
+            return 1;
+        int len = 64 - Long.numberOfLeadingZeros(n);
+        long count = 1;
+        for (int i = 1; i < len; i++) {
+            int half = (i + 1) / 2;
+            count += 1L << (half - 1);
+        }
+        int half = (len + 1) / 2;
+        long prefix = n >> (len - half);
+        long palin = makePalin(prefix, len % 2 == 1);
+        count += (prefix - (1L << (half - 1)));
+        if (palin <= n)
+            ++count;
+        return (int)count;
+    }
+}
