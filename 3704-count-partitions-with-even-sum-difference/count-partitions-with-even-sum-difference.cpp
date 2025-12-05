e630c71d32f7ff0a1b4d42aed13a43469909b690
Eva Albert <44654369+evialbert@users.noreply.github.com>
2025-12-05 13:57:06 +0200

    Time: 0 ms (100.00%) | Memory: 22.4 MB (53.79%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(NULL);
-
-        int count=0;
-        long long right = accumulate(nums.begin(),nums.end(),0LL);
-        long long left = 0;
-        for(int i=0;i<nums.size()-1;i++){
-            left+=nums[i];
-            right-=nums[i];
-            if(abs(left-right)%2==0){
-                count++;
-            }
-        }
-        return count;
+        return (reduce(nums.begin(), nums.end(), 0)&1)?0:nums.size()-1;
     }
 };
