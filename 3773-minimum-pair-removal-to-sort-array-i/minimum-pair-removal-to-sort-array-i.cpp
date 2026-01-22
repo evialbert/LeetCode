8aa94344fddc0e44b496d1ad8ca8a75549706768
Eva Albert <44654369+evialbert@users.noreply.github.com>
2026-01-22 17:54:28 +0200

    Time: 0 ms (100.00%) | Memory: 26.6 MB (61.14%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
-        int ans = 0;
-        while(true) 
-        {
-            vector<int> temp = nums;
-            sort(temp.begin(),temp.end());
-            if(nums==temp) return ans;
-            vector<vector<int>> arr;
-            for(int i=0; i<nums.size()-1; i++) 
-            {
-                int x = nums[i] + nums[i+1];
-                arr.push_back({x,i});
+        int operations = 0;
+
+        while(true){
+            // 1) Check if sorted.
+            bool isSorted = true;
+
+            for(int i = 0; i < nums.size() - 1;i++){
+                if(nums[i] > nums[i+1]){
+                    isSorted = false;
+                }
             }
-            sort(arr.begin(), arr.end());
-            int sum = arr[0][0];
-            int idx = arr[0][1];
-            nums[idx] = sum;
-            nums.erase(nums.begin()+idx+1);
-            ans++;
+            
+            if(isSorted) return operations;
+
+            // 2) Find min pair.
+            int minSum = INT_MAX;
+            int index;
+
+            for(int i = 0; i < nums.size() - 1; i++){
+                int currentSum = nums[i] + nums[i+1];
+                if(minSum > currentSum){
+                    minSum = currentSum;
+                    index = i;
+                }
+            }
+
+            // 3) Replace and merge.
+            nums[index] = minSum;
+            nums.erase(nums.begin() + index + 1);
+            operations++;
         }
-        return ans;
     }
 };
