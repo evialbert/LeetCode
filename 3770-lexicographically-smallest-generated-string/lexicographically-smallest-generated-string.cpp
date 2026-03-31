1765ff111035ec6aa2a58ce1193170e68a2337a1
Eva Albert <44654369+evialbert@users.noreply.github.com>
2026-03-31 19:15:44 +0300

    Time: 30 ms (22.03%) | Memory: 37.8 MB (11.86%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
@@ -1,52 +1,46 @@
 class Solution {
 public:
-    string generateString(string s, string t) 
-    {
-        int n=s.size(),m=t.size(),l=n+m-1;
-        string res(l,'*');
-        //Set T postitions, which is fixed
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='F')
-               continue;
-            for(int j=0,k=i;j<m;j++,k++)
-            {
-                if(res[k]=='*')
-                   res[k]=t[j];
-                else if(res[k]!=t[j])
-                   return "";   
+    string generateString(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+
+        string word(n + m - 1, 'a');
+        vector<int> fixed(n + m - 1, 0);
+
+        for (int i = 0; i < n; i++)
+            if (str1[i] == 'T') {
+                word.replace(i, m, str2);
+
+                for (int j = i; j < i + m; j++)
+                    fixed[j] = 1;
+            }
+
+        for (int i = 0; i < n; i++) {
+            string sub = word.substr(i, m);
+
+            if (str1[i] == 'T') {
+                if (sub != str2)
+                    return "";
+            } 
+            else {
+                if (sub == str2) {
+                    int indx = -1;
+
+                    for (int j = i + m - 1; j >= i; j--) {
+                        if (!fixed[j]) {
+                            indx = j;
+                            break;
+                        }
+                    }
+
+                    if (indx == -1)
+                        return "";
+
+                    word[indx] = 'b';
+                }
             }
         }
-        // Now greedy set * positions
-        vector<bool>pos(l,false);
-        for(int i=0;i<l;i++)
-        {
-            if(res[i]=='*')
-            {
-                res[i]='a';
-                pos[i]=true;
-            }   
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='T')
-               continue;
-            string tmp=res.substr(i,m);
-            if(tmp!=t)
-               continue;
-            bool flag=false;     
-            for(int j=i+m-1;j>=i;j--)
-            {
-                if(pos[j] && res[j]==t[j-i])
-                {
-                    res[j]='b';
-                    flag=true;
-                    break;
-                }
-            }   
-            if(!flag)
-               return "";
-        }
-        return res;
+
+        return word;
     }
 };
