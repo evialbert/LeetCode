9c9d8792a4e697ff337427e99410e1a5e4454e86
Eva Albert <44654369+evialbert@users.noreply.github.com>
2026-04-02 11:48:35 +0300

    Time: 7 ms (100.00%) | Memory: 106 MB (99.57%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,68 +1,33 @@
 class Solution {
 public:
-    int maximumAmount(vector<vector<int>>& coins) {
-        int n = coins.size(), m = coins[0].size();
-        int dp[501][501][3] = {0};
-        if(coins[0][0]>=0){
-            dp[0][0][2] = coins[0][0];
-            dp[0][0][1] = coins[0][0];
-            dp[0][0][0] = coins[0][0];
-        }
-        else {
-            dp[0][0][2] = coins[0][0];
-            dp[0][0][1] = 0;
-            dp[0][0][0] = 0;
-        }
-        for(int i=1;i<n;i++)
-        {
-            if(coins[i][0]>=0){
-                dp[i][0][2] = dp[i-1][0][2] + coins[i][0]; 
-                dp[i][0][1] = dp[i-1][0][1] + coins[i][0]; 
-                dp[i][0][0] = dp[i-1][0][0] + coins[i][0]; 
+    int maximumAmount(vector<vector<int>>& g) {
+        int dp[500][3];
+        const int h = g.size(), w = g[0].size();
+
+        fill_n(dp[0], 3 * w, -500'000);
+        fill_n(dp[0], 3, 0);
+
+        for (int y = 0; y != h; ++y) {
+            {
+                constexpr int x = 0;
+                int top[]{dp[x][0], dp[x][1], dp[x][2]};
+                int v = g[y][x], v0 = max(v, 0);
+                dp[x][0] = v + top[0];
+                dp[x][1] = max(v + top[1], v0 + top[0]);
+                dp[x][2] = max(v + top[2], v0 + top[1]);
             }
-            else {
-                dp[i][0][2] = dp[i-1][0][2] + coins[i][0];
-                dp[i][0][1] = max(dp[i-1][0][1] + coins[i][0], dp[i-1][0][2]);
-                dp[i][0][0] = max(dp[i-1][0][0] +  coins[i][0], dp[i-1][0][1]);
-            }
-        }
-        for(int i=1;i<m;i++)
-        {
-            if(coins[0][i]>=0){
-                dp[0][i][2] = dp[0][i-1][2] + coins[0][i]; 
-                dp[0][i][1] = dp[0][i-1][1]  +coins[0][i];
-                dp[0][i][0] = dp[0][i-1][0]  +coins[0][i];
-            }
-            else {
-                dp[0][i][2] = dp[0][i-1][2] + coins[0][i];
-                dp[0][i][1] = max(dp[0][i-1][1] + coins[0][i], dp[0][i-1][2]);
-                dp[0][i][0] = max(dp[0][i-1][0] +  coins[0][i], dp[0][i-1][1]);
+
+            for (int x = 1; x != w; ++x) {
+                int top[]{dp[x][0], dp[x][1], dp[x][2]};
+                int v = g[y][x], v0 = max(v, 0);
+                dp[x][0] = max(v + dp[x - 1][0], v + top[0]);
+                dp[x][1] = max(max(v + dp[x - 1][1], v + top[1]),
+                               max(v0 + dp[x - 1][0], v0 + top[0]));
+                dp[x][2] = max(max(v + dp[x - 1][2], v + top[2]),
+                               max(v0 + dp[x - 1][1], v0 + top[1]));
             }
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(coins[i][j]>=0){
-                    dp[i][j][2] = max(dp[i-1][j][2], dp[i][j-1][2]) + coins[i][j]; 
-                    dp[i][j][1] = max(dp[i-1][j][1], dp[i][j-1][1]) + coins[i][j]; 
-                    dp[i][j][0] = max(dp[i-1][j][0], dp[i][j-1][0]) + coins[i][j]; 
-                }
-                else {
-                    dp[i][j][2] = max(dp[i-1][j][2], dp[i][j-1][2]) + coins[i][j];
-                    dp[i][j][1] = max(max(dp[i-1][j][1], dp[i][j-1][1]) + coins[i][j], max(dp[i-1][j][2], dp[i][j-1][2]));
-                    dp[i][j][0] = max(max(dp[i-1][j][0], dp[i][j-1][0]) +  coins[i][j], max(dp[i-1][j][1], dp[i][j-1][1]));
-                }
-            }
-        }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         for(int k=0;k<3;k++)
-        //         {
-        //             cout<<dp[i][j][k]<<",";
-        //         }
-        //         cout<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return max(dp[n-1][m-1][0], max(dp[n-1][m-1][1], dp[n-1][m-1][2]));
+
+        return dp[w - 1][2];
     }
 };
