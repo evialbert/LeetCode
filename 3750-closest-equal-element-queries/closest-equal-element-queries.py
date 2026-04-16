f7905a2d48c89f7e75c507496d5f10beba09b99b
Eva Albert <44654369+evialbert@users.noreply.github.com>
2026-04-16 22:58:28 +0300

    Time: 640 ms (6.25%) | Memory: 84.3 MB (5.47%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.py
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.py
@@ -1,62 +1,24 @@
 class Solution:
     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+
+        mp1 = collections.defaultdict(list)
+        mp2 = collections.defaultdict(lambda: defaultdict(int))
+
+        for index, value in enumerate(nums):
+            mp1[value].append(index)
+            mp2[value][index] = len(mp1[value])-1
+          
+        ans = []    
+
+        for q in queries:
+            L = mp1[nums[q]]
+            index = mp2[nums[q]][q]
+            x, y = abs(L[index]-L[(index+1) % len(L)]), abs(L[index]-L[(index-1) % len(L)])
+            if x == 0 and y == 0:
+                ans.append(-1)
+            else:    
+                ans.append(min(x, y, len(nums)-x, len(nums)-y))
+             
+        return ans    
+
         
-        def binary_search(all_index , index):
-
-            start , end = 0 , len(all_index)
-
-            while start < end:
-
-                mid = (start + end) // 2
-
-                if all_index[mid] == index:
-                    return mid
-                if all_index[mid] < index:
-                    start = mid + 1
-
-                if all_index[mid] > index:
-                    end = mid - 1
-            
-            return start
-
-        result = []
-        
-        hashmap = defaultdict(list)
-        
-        for index in range(len(nums)):
-            
-            hashmap[nums[index]].append(index)
-        
-        for index in queries:
-            
-            all_index = hashmap[nums[index]]
-
-            if len(all_index) == 1:
-                result.append(-1)
-            else:
-
-                current_index = binary_search(all_index , index)
-
-                min_distance = float('inf')
-
-                if 0 < current_index < len(all_index) - 1:
-
-                    min_distance = min(min_distance , abs(all_index[current_index + 1] - index))
-
-                    min_distance = min(min_distance , abs(index - all_index[current_index - 1]))
-                else:
-                    if current_index == 0:
-
-                        min_distance = min(min_distance , abs(all_index[current_index + 1] - index))
-
-                        min_distance = min(min_distance , abs(index + len(nums) - all_index[-1]))
-                        
-                    if current_index == len(all_index) - 1:
-                        
-                        min_distance = min(min_distance , abs(all_index[0] + len(nums) - all_index[-1]))
-                        
-                        min_distance = min(min_distance , abs(index - all_index[current_index - 1] ))
-
-                result.append(min_distance)
-        
-        return result
