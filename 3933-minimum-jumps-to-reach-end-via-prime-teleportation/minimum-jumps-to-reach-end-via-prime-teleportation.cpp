26389fb452a732a1e340c2b1872204900f92894d
Eva Albert <44654369+evialbert@users.noreply.github.com>
2026-05-08 18:10:07 +0300

    Time: 96 ms (100.00%) | Memory: 274.3 MB (82.66%) - LeetSync

---
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -0,0 +1,72 @@
+class Solution {
+    static const int n = 1000005;
+    static inline int prime[n];
+    static inline int version = 0;
+    static inline bool init = []() {
+        prime[0] = -1;
+        prime[1] = -1;
+        for (int i = 2; i <= 1000; i++)
+            if (prime[i] == 0)
+                for (int j = i * i; j < n; j += i)
+                    prime[j] = -1;
+        return true;
+    }();
+
+public:
+    int minJumps(vector<int>& nums) {
+        int len = nums.size();
+        int max = *max_element(nums.begin(), nums.end());
+
+        vector<int> head(max + 1, -1);
+        vector<int> next(len);
+
+        for (int i = 0; i < len; i++) {
+            next[i] = head[nums[i]];
+            head[nums[i]] = i;
+        }
+
+        vector<int> dp(len, -1);
+        dp[0] = 0;
+
+        queue<int> queue;
+        queue.push(0);
+
+        version++;
+
+        while (!queue.empty()) {
+            int dq = queue.front();
+            queue.pop();
+
+            if (dq == len - 1)
+                return dp[dq];
+
+            int right = dq + 1;
+            if (right < len && dp[right] == -1) {
+                dp[right] = dp[dq] + 1;
+                queue.push(right);
+            }
+
+            int left = dq - 1;
+            if (left >= 0 && dp[left] == -1) {
+                dp[left] = dp[dq] + 1;
+                queue.push(left);
+            }
+
+            int val = nums[dq];
+            if (prime[val] != -1 && prime[val] != version) {
+                prime[val] = version;
+                for (int i = val; i <= max; i += val) {
+                    for (int j = head[i]; j != -1; j = next[j]) {
+                        if (dp[j] == -1) {
+                            dp[j] = dp[dq] + 1;
+                            queue.push(j);
+                        }
+                    }
+                    head[i] = -1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
